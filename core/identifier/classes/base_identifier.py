223445ed03ea36b9c1342fc88d40d343ac8112e5
erik van widenfelt <ew2789@gmail.com>
2013-10-13 21:22:44 +0200

    fix identifier base to work with plot identifier

--- core/identifier/classes/base_identifier.py
+++ core/identifier/classes/base_identifier.py
@@ -15,8 +15,7 @@ class BaseIdentifier(object):
                  modulus=None, identifier_prefix=None, is_derived=False, add_check_digit=None, using=None,
                  sequence_app_label=None, sequence_model_name=None):
         self.identifier_format = None
-        self.app_name = None
-        self.model_name = None
+        self._identifier_history_model = None
         self._sequence_app_label = None
         self._sequence_model_name = None
         self.padding = None
@@ -34,17 +33,13 @@ class BaseIdentifier(object):
         if 'PROJECT_IDENTIFIER_MODULUS' not in dir(settings):
             modulus = modulus or 7
         self.identifier_format = identifier_format or "{prefix}-{site_code}{device_id}{sequence}"
-        self.app_name = app_name or 'identifier'
-        self.model_name = model_name or 'subjectidentifier'
         self._sequence_app_label = sequence_app_label or 'identifier'
         self._sequence_model_name = sequence_model_name or 'sequence'
         self.padding = padding or 4
         self.modulus = modulus or settings.PROJECT_IDENTIFIER_MODULUS
         self.identifier_prefix = identifier_prefix or settings.PROJECT_IDENTIFIER_PREFIX
+        self.set_identifier_history_model(app_name, model_name)
         self.site_code = site_code or ''
-        # confirm identifier tracking model exists
-        if not get_model(self.app_name, self.model_name):
-            raise ImproperlyConfigured('Identifier tracking model with app_name={0} and model_name={1} does not exist.'.format(self.app_name, self.model_name))
 
     def _set_sequence_app_label(self, value):
         self._sequence_app_label = value
@@ -96,6 +91,16 @@ class BaseIdentifier(object):
         identifier = self.get_identifier_post(identifier, **kwargs)
         return identifier
 
+    def set_identifier_history_model(self, app_name, model_name):
+        app_name = app_name or 'identifier'
+        model_name = model_name or 'subjectidentifier'
+        self._identifier_history_model = get_model(app_name, model_name)
+        if not self._identifier_history_model:
+            raise ImproperlyConfigured('Identifier history model with app_name={0} and model_name={1} does not exist.'.format(app_name, model_name))
+
+    def get_identifier_history_model(self):
+        return self._identifier_history_model
+
     def get_check_digit(self, base_new_identifier):
         """Adds a check digit base on the integers in the identifier."""
         if not self.add_check_digit:
@@ -105,6 +110,21 @@ class BaseIdentifier(object):
             return "{base}-{check_digit}".format(
                 base=base_new_identifier,
                 check_digit=check_digit.calculate(int(re.search('\d+', base_new_identifier.replace('-', '')).group(0)), self.modulus))
+
+    def _get_identifier_history_model_options(self):
+        """Returns the options to create a new history model instance."""
+        options = {
+            'identifier': str(uuid.uuid4()),
+            'padding': self.padding,
+            "is_derived": self.is_derived,
+            "sequence_app_label": self._get_sequence_app_label(),
+            "sequence_model_name": self._get_sequence_model_name()}
+        options.update(self.get_identifier_history_model_options())
+        return options
+
+    def get_identifier_history_model_options(self):
+        """Users may override to add additional options."""
+        return {}
 
     def get_identifier(self, add_check_digit=None, **kwargs):
         """ Returns a formatted identifier based on the identifier format and the dictionary
@@ -122,13 +142,7 @@ class BaseIdentifier(object):
         format_options = self._get_identifier_prep(**kwargs)
         if self.is_derived == None:
             raise AttributeError('Instance attribute is_derived has not been set. Options are True/False')
-        IdentifierModel = get_model(self.app_name, self.model_name)
-        self.identifier_model = IdentifierModel.objects.using(self.using).create(
-            identifier=str(uuid.uuid4()),
-            padding=self.padding,
-            is_derived=self.is_derived,
-            sequence_app_label=self._get_sequence_app_label(),
-            sequence_model_name=self._get_sequence_model_name())
+        self.identifier_model = self.get_identifier_history_model().objects.using(self.using).create(**self._get_identifier_history_model_options())
         format_options.update(sequence=self.identifier_model.formatted_sequence)
         if self.is_derived:
             # if derived, does not use a sequence number -- that is the sequence is in the base identifier,
