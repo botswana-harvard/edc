0d4f23c4de0ad0a433e9c16b9e54112f6fc1037e
SirOne Pharatlhatlhe <sirone@One.local>
2013-10-18 11:18:08 +0200

    FIX: dispatch stuff

--- device/dispatch/urls.py
+++ device/dispatch/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^return/(?P<producer>[a-z0-9\-\_\.]+)/', 'return_items'),
     url(r'^return/(?P<identifier>\w+)/', 'return_households', name='return_household'),
+    url(r'^return/(?P<identifier>\w+)/', 'return_households', name='return_household'),
     )
