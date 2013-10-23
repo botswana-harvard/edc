b83d38a6648937c303134a481ac3b4f7b0ab89ec
erik van widenfelt <ew2789@gmail.com>
2013-10-23 12:41:54 +0200

    add RevisionField to BaseUuidModel

--- base/model/models/base_uuid_model.py
+++ base/model/models/base_uuid_model.py
@@ -1,4 +1,5 @@
-from ..fields import UUIDAutoField
+from ..fields import UUIDAutoField, RevisionField
+
 from .base_model import BaseModel
 
 
@@ -7,6 +8,8 @@ class BaseUuidModel(BaseModel):
     """Base model class for all models using an UUID and not an INT for the primary key. """
 
     id = UUIDAutoField(primary_key=True)
+
+    revision = RevisionField()
 
     class Meta:
         abstract = True
