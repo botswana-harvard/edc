7820bdf50ed752e5c95191a03b33f4a7d1b8c4ba
melissa kewagamang <mkewagamang@bhp.org.bw>
2013-10-24 09:49:00 +0200

    Erik add revision migrations

---
+++ subject/subject_config/migrations/0002_auto__add_field_subjectconfiguration_revision.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'SubjectConfiguration.revision'
+        db.add_column('bhp_subject_config_subjectconfiguration', 'revision',
+                      self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'SubjectConfiguration.revision'
+        db.delete_column('bhp_subject_config_subjectconfiguration', 'revision')
+
+
+    models = {
+        'subject_config.subjectconfiguration': {
+            'Meta': {'object_name': 'SubjectConfiguration', 'db_table': "'bhp_subject_config_subjectconfiguration'"},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'default_appt_type': ('django.db.models.fields.CharField', [], {'default': "'clinic'", 'max_length': '10'}),
+            'hostname_created': ('django.db.models.fields.CharField', [], {'default': "'silverapple'", 'max_length': '50', 'db_index': 'True', 'blank': 'True'}),
+            'hostname_modified': ('django.db.models.fields.CharField', [], {'default': "'silverapple'", 'max_length': '50', 'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.CharField', [], {'max_length': '36', 'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'revision': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'subject_identifier': ('django.db.models.fields.CharField', [], {'max_length': '36'}),
+            'user_created': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250', 'db_index': 'True'}),
+            'user_modified': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '250', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['subject_config']
