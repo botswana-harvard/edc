22843cf71aa0b818f5cfac9209a5c79a48915742
erik van widenfelt <ew2789@gmail.com>
2013-10-24 20:39:49 +0200

    update admin

--- export/admin/export_history_admin.py
+++ export/admin/export_history_admin.py
@@ -6,7 +6,7 @@ from ..models import ExportHistory
 class ExportHistoryAdmin (admin.ModelAdmin):
 
     date_hierarchy = 'export_datetime'
-    list_filter = ('export_datetime')
+    list_filter = ('export_datetime', )
     search_fields = ('export_pk',)
 
 admin.site.register(ExportHistory, ExportHistoryAdmin)
