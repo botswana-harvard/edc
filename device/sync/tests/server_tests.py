81895f0aa7e70efc0585f5b0dd3a4f2b4172a1cf
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2013-10-25 15:37:34 +0200

    testing server synchronizing link

--- device/sync/tests/server_tests.py
+++ device/sync/tests/server_tests.py
@@ -1,3 +1,4 @@
+import socket
 from django.conf import settings
 from tastypie.models import ApiKey
 from django.contrib.auth.models import User
@@ -11,24 +12,30 @@ class ServerTests(BaseSyncDeviceTests):
 
         
     def test_server_settings(self):
-        if 'MIDDLE_MAN' in dir(settings) and settings.MIDDLE_MAN:
+        if 'MIDDLE_MAN' in dir(settings):
             settings.MIDDLE_MAN = False
-        self.assertRaises(TypeError, lambda: MiddleManTransactionFactory())
-
-        
+        self.assertTrue(settings.DEVICE_ID == '99')
+       
     def test_tastypie_synchronizing_link(self):
         producer = 'bcpp039-bhp066'
         app_name = 'bcpp'
         producer_instance = ProducerFactory(name=producer, settings_key=producer, url='http://localhost:8000/')
-        if 'MIDDLE_MAN' in dir(settings) and not settings.MIDDLE_MAN:
-            settings.MIDDLE_MAN = True
+        if 'MIDDLE_MAN' in dir(settings):
+            settings.MIDDLE_MAN = False
         self.assertEqual(User.objects.all().count(),1)
         self.assertEqual(ApiKey.objects.all().count(),1)
         self.denies_anonymous_acess(producer, app_name)
-        print 'Number of OUTGOING TRANSACTIONS = {0}'.format(OutgoingTransaction.objects.all().count())
-        response = self.client.get('/bhp_sync/consume/'+producer+'/'+app_name+'/', follow=True)
+        print 'Number of OUTGOING TRANSACTIONS = {0}'.format(OutgoingTransaction.objects.all().count())        
         #print response
-        self.assertTrue(str(response).find('/bhp_sync/api_otmr/outgoingtransaction') != -1)
+        if not socket.gethostname()+'-bhp066' in settings.MIDDLE_MAN_LIST:
+            response = self.client.get('/bhp_sync/consume/'+producer+'/'+app_name+'/', follow=True)
+            self.assertTrue(str(response).find('/bhp_sync/api_otsr/outgoingtransaction') != -1)
+            self.assertFalse(str(response).find('/bhp_sync/api_mmtr/middlemantransaction') != -1)           
+        elif socket.gethostname()+'-bhp066' in settings.MIDDLE_MAN_LIST:
+            settings.MIDDLE_MAN_LIST.append(socket.gethostname()+'-bhp066')
+            response = self.client.get('/bhp_sync/consume/'+producer+'/'+app_name+'/', follow=True)
+            self.assertTrue(str(response).find('/bhp_sync/api_mmtr/middlemantransaction') != -1)
+            self.assertFalse(str(response).find('/bhp_sync/api_otsr/outgoingtransaction') != -1)     
         self.assertEqual(response.status_code, 200)
         self.assertRedirects(response,'/dispatch/bcpp/sync/'+producer+'/')
         
