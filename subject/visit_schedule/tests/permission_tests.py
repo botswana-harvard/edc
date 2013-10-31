940ad032c19a82977f1c6338a1e13b63d454ea7c
erik van widenfelt <ew2789@gmail.com>
2013-10-31 11:43:48 +0200

    add management command, update tests, etc.

--- subject/visit_schedule/tests/permission_tests.py
+++ subject/visit_schedule/tests/permission_tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.db.models import get_app, get_models
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 
@@ -30,14 +31,14 @@ class PermissionTests(TestCase):
         self.group = Group.objects.create(name='field_staff')
 
     def test_adds_permissions1(self):
-        permissions = Permissions(['T0'], 'field_staff', ['add'])
+        permissions = Permissions('field_staff', ['add'], visit_definition_codes=['T0'])
         permissions.replace()
         group = Group.objects.get(name='field_staff')
         self.assertGreater(group.permissions.all().count(), 0)
 
     def test_adds_permissions2(self):
         """Adds permissions to the group for just add."""
-        permissions = Permissions(['T0'], 'field_staff', ['add'])
+        permissions = Permissions('field_staff', ['add'], visit_definition_codes=['T0'])
         permissions.replace()
         group = Group.objects.get(name='field_staff')
         self.assertGreater(group.permissions.filter(codename__icontains='add_').count(), 0)
@@ -49,7 +50,7 @@ class PermissionTests(TestCase):
         codes = ['T0']
         visit_definitions = VisitDefinition.objects.filter(code__in=codes)
         entry_count = Entry.objects.filter(visit_definition__in=visit_definitions).count()
-        permissions = Permissions(['T0'], 'field_staff', ['add', 'change'])
+        permissions = Permissions('field_staff', ['add', 'change'], visit_definition_codes=codes)
         permissions.replace()
         group = Group.objects.get(name='field_staff')
         self.assertEqual(group.permissions.filter(codename__icontains='add_').count(), entry_count)
@@ -61,7 +62,7 @@ class PermissionTests(TestCase):
         codes = ['T0']
         visit_definitions = VisitDefinition.objects.filter(code__in=codes)
         entry_count = Entry.objects.filter(visit_definition__in=visit_definitions).count()
-        permissions = Permissions(codes, 'field_staff', ['add', 'change', 'delete'])
+        permissions = Permissions('field_staff', ['add', 'change', 'delete'], visit_definition_codes=codes)
         permissions.replace()
         group = Group.objects.get(name='field_staff')
         self.assertEqual(group.permissions.filter(codename__icontains='add_').count(), entry_count)
@@ -73,7 +74,7 @@ class PermissionTests(TestCase):
         codes = ['T1']
         visit_definitions = VisitDefinition.objects.filter(code__in=codes)
         entry_count = Entry.objects.filter(visit_definition__in=visit_definitions).count()
-        permissions = Permissions(codes, 'field_staff', ['add', 'change', 'delete'])
+        permissions = Permissions('field_staff', ['add', 'change', 'delete'], visit_definition_codes=codes)
         permissions.replace()
         group = Group.objects.get(name='field_staff')
         self.assertEqual(group.permissions.filter(codename__icontains='add_').count(), entry_count)
@@ -86,7 +87,7 @@ class PermissionTests(TestCase):
         visit_definitions = VisitDefinition.objects.filter(code__in=codes)
         entries = Entry.objects.filter(visit_definition__in=visit_definitions)
         entry_count = len(list(set([entry.content_type_map.content_type_id for entry in entries])))
-        permissions = Permissions(codes, 'field_staff', ['add', 'change', 'delete'])
+        permissions = Permissions('field_staff', ['add', 'change', 'delete'], visit_definition_codes=codes)
         permissions.replace()
         group = Group.objects.get(name='field_staff')
         self.assertEqual(group.permissions.filter(codename__icontains='add_').count(), entry_count)
@@ -95,7 +96,30 @@ class PermissionTests(TestCase):
 
     def test_adds_permissions7(self):
         """Creates a group if it does not exist."""
-        permissions = Permissions(['T0'], 'field_staff_team', ['add'])
+        codes = ['T0']
+        permissions = Permissions('field_staff_team', ['add'], visit_definition_codes=codes)
         permissions.replace()
         group = Group.objects.get(name='field_staff_team')
         self.assertGreater(group.permissions.all().count(), 0)
+
+    def test_adds_permissions8(self):
+        """Adds permissions for all models in the app"""
+        permissions = Permissions('field_staff_team', ['add'], app_label='testing')
+        permissions.replace()
+        group = Group.objects.get(name='field_staff_team')
+        app = get_app('testing')
+        models = get_models(app)
+        self.assertEquals(group.permissions.all().count(), len(models))
+
+    def test_adds_permissions9(self):
+        """Adds permissions for specified list of models in the app."""
+        permissions = Permissions('field_staff', ['add', 'change'], app_label='testing', models=['testvisit', 'TestScheduledModel'])
+        permissions.replace()
+        self.assertEqual(self.group.permissions.filter(codename='add_testvisit').count(), 1)
+        self.assertEqual(self.group.permissions.filter(codename='add_testscheduledmodel').count(), 1)
+        self.assertEqual(self.group.permissions.filter(codename='change_testvisit').count(), 1)
+        self.assertEqual(self.group.permissions.filter(codename='change_testscheduledmodel').count(), 1)
+
+    def test_adds_permissions10(self):
+        """Raises error if model not in app"""
+        self.assertRaises(AttributeError, Permissions, 'field_staff', ['add', 'change'], app_label='testing', models=['testvisit', 'BadDogModel'])
