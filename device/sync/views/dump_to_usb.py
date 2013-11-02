77f18e6af13e2c3339d06ae1d531eee8b5fd20bb
erik van widenfelt <ew2789@gmail.com>
2013-11-02 17:44:59 +0200

    PEP8

--- device/sync/views/dump_to_usb.py
+++ device/sync/views/dump_to_usb.py
@@ -1,41 +1,44 @@
 import os
 import platform
+
 from datetime import datetime
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ValidationError
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from ..models import OutgoingTransaction
+
 from ..classes import SerializeToTransaction
+from ..models import OutgoingTransaction
 
 
 @login_required
 def dump_to_usb(request, **kwargs):
-    app_name = kwargs.get('app_name',None)
+    app_name = kwargs.get('app_name', None)
     usb_path = None
     if platform.system() == 'Darwin':
-        usb_path = '/Volumes/'+app_name+'_usb/'
+        usb_path = '/Volumes/' + app_name + '_usb/'
     else:
-        usb_path = '/media/'+app_name+'_usb/'
+        usb_path = '/media/' + app_name + '_usb/'
     if not app_name:
         raise ValidationError('app_name cannot be None')
     try:
-        f = open(usb_path+app_name+'_'+str(datetime.now().strftime("%Y-%m-%d%H%M"))+'.json','w')
+        f = open(usb_path + app_name + '_' + str(datetime.now().strftime("%Y-%m-%d%H%M")) + '.json', 'w')
     except:
-        raise ValidationError('Please insert a usb device named \'{0}_usb\', currently using USB_PATH \'{1}\''.format(app_name,usb_path))
-    outgoing = OutgoingTransaction.objects.filter(is_consumed_server=False)
+        raise ValidationError('Please insert a usb device named \'{0}_usb\', currently using USB_PATH \'{1}\''.format(app_name, usb_path))
+    outgoing_transactions = OutgoingTransaction.objects.filter(is_consumed_server=False)
     serializer = SerializeToTransaction()
-    success = serializer.serialize_to_file(outgoing, f, True)
+    success = serializer.serialize_to_file(outgoing_transactions, f, True)
     f.close()
-    for ot in outgoing:
-        ot.is_consumed_middleman = True
-        ot.consumer = usb_path
-        ot.save()
+    for outgoing_transaction in outgoing_transactions:
+        outgoing_transaction.is_consumed_middleman = True
+        outgoing_transaction.consumer = usb_path
+        outgoing_transaction.save()
     if not success:
         messages.add_message(request, messages.ERROR, 'Serialization to path \'{0}\' failed'.format(usb_path))
     else:
-        messages.add_message(request, messages.INFO, 'Serialized \'{0}\' \'{1}\' transactions to \'{2}\''.format(outgoing.count(),'OutgoingTransactions',usb_path))  
+        messages.add_message(request, messages.INFO, 'Serialized \'{0}\' \'{1}\' transactions to \'{2}\''.format(outgoing_transactions.count(), 'OutgoingTransactions', usb_path))
     return render_to_response(
         'dump_to_usb.html',
          context_instance=RequestContext(request)
