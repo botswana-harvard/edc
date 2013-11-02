808d7ee9a9d7138d155dd5150067bdedf941bd27
erik van widenfelt <ew2789@gmail.com>
2013-11-02 16:11:02 +0200

    remove view on import

--- import/admin/upload_transaction_file_admin.py
+++ import/admin/upload_transaction_file_admin.py
@@ -8,7 +8,7 @@ class UploadTransactionFileAdmin(admin.ModelAdmin):
 
     form = UploadTransactionFileForm
     date_hierarchy = 'created'
-    list_display = ('file_name', 'consumed', 'not_consumed', 'created', 'user_created', 'hostname_created')
+    list_display = ('file_name', 'consumed', 'not_consumed', 'created', 'producer', 'user_created', 'hostname_created')
     list_filter = ('created', 'hostname_created')
 
 admin.site.register(UploadTransactionFile, UploadTransactionFileAdmin)
