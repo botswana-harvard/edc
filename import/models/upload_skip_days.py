df73dcd06dc5b3074274784d0bc8356684ea1989
SirOne Pharatlhatlhe <sirone@One.local>
2013-11-05 10:25:15 +0200

    skip dates migration

--- import/models/upload_skip_days.py
+++ import/models/upload_skip_days.py
@@ -1,11 +1,15 @@
+from datetime import date
+
 from django.db import models
 
 from edc.base.model.models import BaseModel
 
 class UploadSkipDays(BaseModel):
 
-    skip_date = models.DateField(unique=True)
+    skip_date = models.DateField(default=date.today(), unique=True)
 
+    objects = models.Manager()
+    
     def save(self, *args, **kwargs):
         super(UploadSkipDays, self).save(*args, **kwargs)
         
