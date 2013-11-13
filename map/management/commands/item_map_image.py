a2d067143f30f09207149def655f81bfe25fad51
ckgathi <ckgathi@bhp.org.bw>
2013-11-13 12:19:12 +0200

    upload maps to database

--- map/management/commands/item_map_image.py
+++ map/management/commands/item_map_image.py
@@ -7,6 +7,7 @@ from django.db.models import Q
 from django.conf import settings
 from ...classes import site_mappers
 from ...exceptions import MapperError
+from database_storage import DatabaseStorage
 
         
 class Command(BaseCommand):
@@ -28,49 +29,93 @@ class Command(BaseCommand):
             mapper = site_mappers.get_registry(mapper_name)()
             letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
                     "O", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-            #items = mapper.get_item_model_cls().objects.filter(**{mapper.map_area_field_attr: mapper_name})
             items = mapper.get_item_model_cls().objects.filter(Q(**{mapper.map_area_field_attr: mapper_name, mapper.item_selected_field: 2}) | Q(**{mapper.map_area_field_attr: mapper_name, mapper.item_selected_field: 1}))
-            
             landmarks = mapper.get_landmarks()
             url_str = ''
+            file_name_18 = ''
+            file_name_17 = ''
+            file_name_16 = ''
             folder = settings.MEDIA_ROOT
             count = 0
             for item in items:
-                zoom_level = 0
-                zoom = 18
-                while zoom_level < 3:
-                    url_str = 'http://maps.google.com/maps/api/staticmap?size=640x600&maptype=satellite&scale:2&format=png32&zoom=' + str(zoom) + '&center='
-                    url_str += str(getattr(item, mapper.target_gps_lat_field_attr)) + ',' + str(getattr(item, mapper.target_gps_lon_field_attr)) + '&'
-                    lmarks = []
-                    for mark in landmarks:
-                        lat = getattr(item, mapper.target_gps_lat_field_attr)
-                        lon = getattr(item, mapper.target_gps_lon_field_attr)
-                        dist = mapper.gps_distance_between_points(lat, lon, mark[1], mark[2])
-                        lmarks.append([dist, landmarks[0], mark[1], mark[2]])
-                    lmark = sorted(lmarks,key=itemgetter(0))
-                    markers_l = mapper.make_dictionary(letters, lmark)
-                    markers_str = ''
-                    for key, value in markers_l.iteritems():
-                        if key:
-                            markers_str += 'markers=color:blue%7Clabel:' + key + '%7C' + str(value[2]) + ',' + str(value[3]) + '&'
-                    url_str += markers_str
-                    url_str += 'markers=color:red%7C' + str(getattr(item, mapper.target_gps_lat_field_attr)) + ',' + str(getattr(item, mapper.target_gps_lon_field_attr)) + '&sensor=false'
-                    name = getattr(item, mapper.get_identifier_field_attr())
-                    if zoom == 18:
-                        file_name = folder + '/' + name + '_18.jpg'
-                    elif zoom == 17:
-                        file_name = folder + '/' + name + '_17.jpg'
-                    else:
-                        file_name = folder + '/' + name + '.jpg'
-                    urlretrieve(url_str, file_name)
-                    sleep(5)
-                    print "The image at zoom level: " + str(zoom) + " of plot: " + str(name) + " is done"
-                    zoom -= 1
-                    zoom_level += 1
-                item.uploaded_map = name + '.jpg'
-                try:
-                    item.save()
-                except Exception as e:
-                    pass
-                count += 1
-                print str((count/float(len(items)))*100) + ' percent done! only ' + str(len(items) - count) + ' more pictures to download'
+                if settings.DEVICE_ID == '99' | settings.DEVICE_ID == '71' | settings.DEVICE_ID == '70':
+                    zoom_level = 0
+                    zoom = 18
+                    while zoom_level < 3:
+                        url_str = 'http://maps.google.com/maps/api/staticmap?size=640x600&maptype=satellite&scale:2&format=png32&zoom=' + str(zoom) + '&center='
+                        url_str += str(getattr(item, mapper.target_gps_lat_field_attr)) + ',' + str(getattr(item, mapper.target_gps_lon_field_attr)) + '&'
+                        lmarks = []
+                        for mark in landmarks:
+                            lat = getattr(item, mapper.target_gps_lat_field_attr)
+                            lon = getattr(item, mapper.target_gps_lon_field_attr)
+                            dist = mapper.gps_distance_between_points(lat, lon, mark[1], mark[2])
+                            lmarks.append([dist, landmarks[0], mark[1], mark[2]])
+                        lmark = sorted(lmarks,key=itemgetter(0))
+                        markers_l = mapper.make_dictionary(letters, lmark)
+                        markers_str = ''
+                        for key, value in markers_l.iteritems():
+                            if key:
+                                markers_str += 'markers=color:blue%7Clabel:' + key + '%7C' + str(value[2]) + ',' + str(value[3]) + '&'
+                        url_str += markers_str
+                        url_str += 'markers=color:red%7C' + str(getattr(item, mapper.target_gps_lat_field_attr)) + ',' + str(getattr(item, mapper.target_gps_lon_field_attr)) + '&sensor=false'
+                        name = getattr(item, mapper.get_identifier_field_attr())
+                        if zoom == 18:
+                            file_name_18 = folder + '/' + name + '_18.jpg'
+                            urlretrieve(url_str, file_name_18)
+                            sleep(5)
+                        elif zoom == 17:
+                            file_name_17 = folder + '/' + name + '_17.jpg'
+                            file_name_18 = folder + '/' + name + '_18.jpg'
+                            urlretrieve(url_str, file_name_17)
+                        else:
+                            file_name_16 = folder + '/' + name + '.jpg'
+                            urlretrieve(url_str, file_name_16)
+                            sleep(5)
+                        print "The image at zoom level: " + str(zoom) + " of plot: " + str(name) + " is done"
+                        zoom -= 1
+                        zoom_level += 1
+                    storage = DatabaseStorage(settings.DB_FILES)
+                    with open(file_name_18, 'rb') as f:
+                        item.uploaded_map_18 = storage.save(name + '_18.jpg', f)
+    
+                    with open(file_name_17, 'rb') as f1:
+                        item.uploaded_map_17 = storage.save(name + '_17.jpg', f1)
+        
+                    with open(file_name_16, 'rb') as f2:
+                        item.uploaded_map_16 = storage.save(name + '.jpg', f2)
+                    try:
+                        item.save()
+                    except Exception as e:
+                        pass
+                    count += 1
+                    print str((count/float(len(items)))*100) + ' percent done! only ' + str(len(items) - count) + ' more pictures to download'
+                else:
+                    for item in items:
+                        if settings.DEVICE_ID == '99' | settings.DEVICE_ID == '71' | settings.DEVICE_ID == '70':
+                            zoom_level = 0
+                            zoom = 18
+                            while zoom_level < 3:
+                                name = getattr(item, mapper.get_identifier_field_attr())
+                                if zoom == 18:
+                                    file_name_18 = folder + '/' + name + '_18.jpg'
+                                elif zoom == 17:
+                                    file_name_17 = folder + '/' + name + '_17.jpg'
+                                else:
+                                    file_name_16 = folder + '/' + name + '.jpg'
+                                print "The image at zoom level: " + str(zoom) + " of plot: " + str(name) + " is done"
+                                zoom -= 1
+                                zoom_level += 1
+                            with open(file_name_18, 'rb') as f:
+                                item.uploaded_map_18 = name + '_18.jpg'
+            
+                            with open(file_name_17, 'rb') as f1:
+                                item.uploaded_map_17 = name + '_17.jpg'
+                
+                            with open(file_name_16, 'rb') as f2:
+                                item.uploaded_map_16 = name + '.jpg'
+                            try:
+                                item.save()
+                            except Exception as e:
+                                pass
+                            count += 1
+                            print str((count/float(len(items)))*100) + ' percent done! only ' + str(len(items) - count) + ' more pictures linked to their images'
