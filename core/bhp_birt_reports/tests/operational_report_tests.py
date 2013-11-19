9156d5234247079b44d56ffe81a9c7a1740dbdb4
SirOne Pharatlhatlhe <sirone@One.local>
2013-11-19 13:06:01 +0200

    operational report test

---
+++ core/bhp_birt_reports/tests/operational_report_tests.py
@@ -0,0 +1,18 @@
+from datetime import date
+from django.test import TestCase
+from ..classes import OperataionalReportUtilities
+
+
+class OperationalReportTests(TestCase):
+
+    def setUp(self):
+        pass
+        
+    def test_date_format(self):
+        utilities = OperataionalReportUtilities()
+        self.assertEqual(utilities.date_format_utility('YYYY/MM/DD', '2013/10/15'), date(2013,10,15))
+        self.assertEqual(utilities.date_format_utility('', '2013/10/15'), date(2013,10,15))
+        self.assertEqual(utilities.date_format_utility('2013-11-15', '2013/10/15'), date(2013,11,15))
+        self.assertEqual(utilities.date_format_utility('2013/11/17', '2013/10/15'), date(2013,11,17))
+        self.assertRaises(TypeError, utilities.date_format_utility, '2013:11:17', '2013/10/15')
+        
