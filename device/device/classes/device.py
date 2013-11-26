e5b9482399f69faa965191d28df16c551d934356
SirOne Pharatlhatlhe <sirone@One.local>
2013-11-26 10:01:29 +0200

    Fix to method is_producer_name_server()

--- device/device/classes/device.py
+++ device/device/classes/device.py
@@ -59,7 +59,7 @@ class Device(object):
         if not name:
             raise TypeError('argument name cannot be None. Pass the producer name.')
         hostname = name.split('-')[0]
-        if int(hostname[4:]) in self.SERVER_DEVICE_ID_LIST:
+        if hostname.find('bcpp') != -1 and int(hostname[4:]) in self.SERVER_DEVICE_ID_LIST:#TEMP Solution, need to FIX
             return True
         return False
             
