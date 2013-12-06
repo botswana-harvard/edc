f7c6823f88687fe4e0422c2f26581d0ad6a60c11
SirOne Pharatlhatlhe <sirone@One.local>
2013-12-06 12:02:12 +0200

    fixing import issues

--- entry_meta_data/classes/requisition_meta_data_helper.py
+++ entry_meta_data/classes/requisition_meta_data_helper.py
@@ -1,6 +1,6 @@
 from edc.subject.entry.models import LabEntry
 
-from ..models import RequisitionMetaData
+from edc.entry_meta_data.models import RequisitionMetaData
 
 from .base_meta_data_helper import BaseMetaDataHelper
 
