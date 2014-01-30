8ea676ea402db62dbc43b97195afb9907d1a5e83
SirOne Pharatlhatlhe <sirone@One.local>
2014-01-30 12:02:54 +0200

    setup of Study Side on bootup

--- apps/app_configuration/classes/base_app_configuration.py
+++ apps/app_configuration/classes/base_app_configuration.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import ImproperlyConfigured
 from edc.subject.appointment.models import Configuration
-from edc.core.bhp_variables.models import StudySpecific
+from edc.core.bhp_variables.models import StudySpecific, StudySite
 from edc.subject.consent.models import ConsentCatalogue
 
 
@@ -8,11 +9,13 @@ class BaseAppConfiguration(object):
     appointment_configuration = None
     study_variables_setup = None
     consent_catalogue_setup = None
+    study_site_setup = None
 
     def __init__(self):
         self.update_or_create_appointment_setup()
         self.update_or_create_study_variables()
         self.update_or_create_consent_catalogue()
+        self.update_or_create_study_site()
 
     def update_or_create_appointment_setup(self):
 
@@ -34,3 +37,12 @@ class BaseAppConfiguration(object):
             ConsentCatalogue.objects.create(**self.consent_catalogue_setup)
         else:
             ConsentCatalogue.objects.all().update(**self.consent_catalogue_setup)
+
+    def update_or_create_study_site(self):
+        if not StudySite.objects.filter(site_code=self.study_site_setup['site_mapper'].map_code).exists():
+            StudySite.objects.create(site_name=self.study_site_setup['site_mapper'].map_area,
+                                         site_code=self.study_site_setup['site_mapper'].map_code)
+        if StudySite.objects.all().count() > 1:
+            raise ImproperlyConfigured('There has to be only one Study Site record on bhp_variables. Got {0}'.format(StudySite.objects.all().count()))
+
+
