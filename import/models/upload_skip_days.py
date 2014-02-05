d3da622128881c5eb998723696f6b5b1dd166586
Twice Tshwenyane <twicet@gmail.com>
2014-02-05 08:26:39 +0200

    removed the dependency on bcpp

--- import/models/upload_skip_days.py
+++ import/models/upload_skip_days.py
@@ -3,16 +3,17 @@ from datetime import date, timedelta
 from django.db import models
 
 from edc.base.model.models import BaseModel
-from apps.bcpp.choices import COMMUNITIES
+from edc.utils.choices import COMMUNITIES
+
 
 class UploadSkipDays(BaseModel):
 
     skip_date = models.DateField(default=date.today())
-    
+
     identifier = models.CharField(max_length=50, choices=COMMUNITIES)
 
     objects = models.Manager()
-    
+
     def save(self, *args, **kwargs):
 #         print self.identifier.lower()
 #         self.identifier = self.identifier.lower()
@@ -22,36 +23,36 @@ class UploadSkipDays(BaseModel):
         if not self.previous_day_file_uploaded() and not self.skip_previous_day() and not self.first_skip_day_or_upload():
             raise TypeError('Missing Upload file for the previous day from \'{0}\'. Previous day is not set as a SKIP date. Therefore \'{1}\' is not a valid skip date.'.format(self.identifier, self.skip_date))
         super(UploadSkipDays, self).save(*args, **kwargs)
-     
+
     def previous_day_file_uploaded(self):
         from .upload_transaction_file import UploadTransactionFile
         previous = self.skip_date - timedelta(1)
         if UploadTransactionFile.objects.filter(file_date=previous, identifier__iexact=self.identifier).exists():
             return True
         return False
-    
+
     def first_skip_day_or_upload(self):
         from .upload_transaction_file import UploadTransactionFile
         #This is the first upload or skip day record.
         if (self.__class__.objects.all().count() == 0) and (UploadTransactionFile.objects.all().count() == 0):
             return True
         return False
-        
+
     def today_upload_exists(self):
         from .upload_transaction_file import UploadTransactionFile
         if UploadTransactionFile.objects.filter(file_date=self.skip_date, identifier__iexact=self.identifier).exists():
             return True
         return False
-    
+
     def skip_previous_day(self):
         yesterday = self.skip_date - timedelta(1)
         if self.__class__.objects.filter(skip_date=yesterday, identifier__iexact=self.identifier).exists():
             return True
         return False
-            
+
     def __unicode__(self):
         return unicode(self.skip_date)
-        
+
     class Meta:
         app_label = 'import'
         ordering = ('-created', )
