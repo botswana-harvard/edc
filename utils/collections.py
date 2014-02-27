8db5c4ef9d88d487bf67731227174d93e34bbb9b
Twice Tshwenyane <twicet@gmail.com>
2014-02-27 10:44:11 +0200

    fixed a bug in flatten utility method

--- utils/collections.py
+++ utils/collections.py
@@ -1,7 +1,7 @@
 def flatten(nested_list):
     """a method to flatten nested lists"""
     flattened_list = []
-    for item in flattened_list:
+    for item in nested_list:
         if hasattr(item, "__getitem__") and not isinstance(item, basestring):
             flattened_list.extend(flatten(item))
         else:
