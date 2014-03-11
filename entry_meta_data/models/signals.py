af7d9a62aa9b62393801b252a12a866c7336b0c1
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2014-03-11 15:52:12 +0200

    fixing import error on meta_data_entry signals

--- entry_meta_data/models/signals.py
+++ entry_meta_data/models/signals.py
@@ -2,13 +2,13 @@ from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 from edc.subject.visit_tracking.models import BaseVisitTracking
-from edc.entry_meta_data.classes import ScheduledEntryMetaDataHelper, RequisitionMetaDataHelper
 from edc.subject.rule_groups.classes import site_rule_groups
 from edc.subject.registration.models import RegisteredSubject
 
 
 @receiver(post_save, weak=False, dispatch_uid="entry_meta_data_on_post_save")
 def entry_meta_data_on_post_save(sender, instance, raw, created, using, update_fields, **kwargs):
+    from edc.entry_meta_data.classes import ScheduledEntryMetaDataHelper, RequisitionMetaDataHelper
     if isinstance(instance, BaseVisitTracking):
         scheduled_entry_helper = ScheduledEntryMetaDataHelper(instance.appointment, sender)
         scheduled_entry_helper.add_or_update_for_visit()
@@ -36,6 +36,7 @@ def entry_meta_data_on_post_save(sender, instance, raw, created, using, update_f
 @receiver(pre_delete, weak=False, dispatch_uid="entry_meta_data_on_pre_delete")
 def entry_meta_data_on_pre_delete(sender, instance, using, **kwargs):
     """Delete metadata if the visit tracking instance is deleted."""
+    from edc.entry_meta_data.classes import ScheduledEntryMetaDataHelper, RequisitionMetaDataHelper
     if isinstance(instance, BaseVisitTracking):
         scheduled_entry_helper = ScheduledEntryMetaDataHelper(instance.appointment, sender)
         scheduled_entry_helper.delete_for_visit()
