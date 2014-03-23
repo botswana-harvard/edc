29d1554dbae10cb6016214ac2406f5728b509c83
erik van widenfelt <ew2789@gmail.com>
2014-03-23 10:59:19 +0200

    add admins for entry models

--- entry_meta_data/admin/meta_data_admin.py
+++ entry_meta_data/admin/meta_data_admin.py
@@ -14,7 +14,7 @@ class ScheduledEntryMetaDataAdmin(BaseModelAdmin):
     form = ScheduledEntryMetaDataForm
     search_fields = ('registered_subject__subject_identifier', 'entry__visit_definition__code', 'entry__content_type_map__model', 'id')
     list_display = ('registered_subject', 'entry', 'entry_status', 'fill_datetime', 'due_datetime', 'close_datetime', 'created', 'hostname_created')
-    list_filter = ('entry_status', 'entry__visit_definition__code', 'fill_datetime', 'created', 'hostname_created')
+    list_filter = ('entry_status', 'entry__visit_definition__code', 'fill_datetime', 'created', 'user_created', 'hostname_created')
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "registered_subject":
@@ -32,7 +32,7 @@ class RequisitionMetaDataAdmin(BaseModelAdmin):
     form = RequisitionMetaDataForm
     search_fields = ('registered_subject__subject_identifier', 'lab_entry__visit_definition__code', 'lab_entry__panel__name')
     list_display = ('registered_subject', 'lab_entry', 'entry_status', 'fill_datetime', 'due_datetime', 'close_datetime')
-    list_filter = ('entry_status', 'lab_entry__visit_definition__code', 'fill_datetime',)
+    list_filter = ('entry_status', 'lab_entry__visit_definition__code', 'fill_datetime', 'created', 'user_created', 'hostname_created')
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "registered_subject":
--- subject/entry/admin/__init__.py
+++ subject/entry/admin/__init__.py
@@ -1,1 +1,3 @@
-from .main import *
+from .entry_admin import EntryAdmin, EntryInline
+from .lab_entry_admin import LabEntryAdmin, LabEntryInline
+from .requisition_panel_admin import RequisitionPanelAdmin
