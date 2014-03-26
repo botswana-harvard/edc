64e6cb57a10f1b2595f46f37ab568c57146c81a2
erik van widenfelt <ew2789@gmail.com>
2014-03-26 15:58:31 +0200

    move string methods to utils

--- apps/app_configuration/managers/configuration_manager.py
+++ apps/app_configuration/managers/configuration_manager.py
@@ -1,7 +1,6 @@
-from dateutil import parser
-from decimal import Decimal, InvalidOperation
-
 from django.db import models
+
+from edc.utils import string_to_datatype
 
 
 class ConfigurationManager(models.Manager):
@@ -10,46 +9,8 @@ class ConfigurationManager(models.Manager):
         """Returns the attribute value in its original datatype assuming it can be converted."""
         obj = self.get(**kwargs)
         string_value = obj.value.strip(' "')
-        retval = string_value
         if obj.convert:
-            retval = self.convert_from_string(string_value)
-        return retval
-
-    def convert_from_string(self, string_value):
-        string_value = string_value.strip(' "')
-        # try to return as boolean or none
-        retval = string_value
-        if string_value.lower() in ['true', 'false', 'none']:
-            retval = eval(string_value)
-        # try to return as a decimal
-        try:
-            value = Decimal(string_value)
-            if str(value) == string_value:
-                retval = value
-        except ValueError:
-            pass
-        except InvalidOperation:
-            pass
-        # try to return as an integer
-        try:
-            value = int(string_value)
-            if str(value) == string_value:
-                retval = value
-        except ValueError:
-            pass
-        # try to return as date
-        try:
-            value = parser.parse(string_value)
-            if value.strftime('%Y-%m-%d') == string_value:
-                retval = value.date()
-        except ValueError:
-            pass
-        # try to return as datetime
-        try:
-            value = parser.parse(string_value)
-            if value.strftime('%Y-%m-%d %H:%M') == string_value:
-                retval = value
-        except ValueError:
-            pass
-        # otherwise return string value
-        return retval
+            value = string_to_datatype(string_value)
+        else:
+            value = string_value
+        return value
