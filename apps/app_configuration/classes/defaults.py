3d264a6804047bb5c3e748c4292b8fab3d7ecf0a
erik van widenfelt <ew2789@gmail.com>
2014-03-26 14:28:17 +0200

    move global defaults to a default module.

---
+++ apps/app_configuration/classes/defaults.py
@@ -0,0 +1,11 @@
+default_global_configuration = {
+    'dashboard': {
+        'show_not_required_metadata': True,
+        'allow_additional_requisitions': False},
+    'appointment': {
+        'allowed_iso_weekdays': '1234567',
+        'use_same_weekday': True,
+        'default_appt_type': 'default',
+        'appointments_per_day_max': 30,
+        'appointments_days_forward': 8},
+    }
