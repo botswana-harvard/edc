ca9fd7928a8557d9ab2e659ae04a35cc9020311c
melissa kewagamang <mkewagamang@bhp.org.bw>
2014-04-08 16:42:32 +0200

    udpate RS_dashabord, views, templates, meta_data_manager

--- dashboard/subject/views.py
+++ dashboard/subject/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from edc.subject.appointment.models import Appointment
 from edc.subject.entry.models.lab_entry import LabEntry
 from edc.subject.entry.models.entry import Entry
-from edc.entry_meta_data.models import RequisitionMetaData, ScheduledEntryMetaData
+from edc.entry_meta_data.models import RequisitionMetaData
 
 
 def additional_requisition(request):
@@ -30,26 +30,3 @@ def additional_requisition(request):
 
 def _get_param(request, param_key):
     return request.GET[param_key]
-
-
-def additional_entry_form(request):
-    appointment_id = _get_param(request, 'appointment_id')
-    entry_id = _get_param(request, 'entry_order')
-
-    appointment = Appointment.objects.get(pk=appointment_id)
-    entry = Entry.objects.get(pk=entry_id)
-    entry_meta_data = ScheduledEntryMetaData.objects.get(appointment=appointment, entry=entry)
-    entry_meta_data.entry_status = 'NEW'
-    entry_meta_data.save()
-
-    dashboard_type = _get_param(request, 'dashboard_type')
-    dashboard_model = _get_param(request, 'dashboard_model')
-    dashboard_id = _get_param(request, 'dashboard_id')
-    show = _get_param(request, 'show')
-    url = _get_param(request, 'url')
-
-    return redirect(url,
-                    dashboard_type=dashboard_type,
-                    dashboard_model=dashboard_model,
-                    dashboard_id=dashboard_id,
-                    show=show)
