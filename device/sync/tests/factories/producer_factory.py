41311b97b9e556fcb33b31062982fe0f3d32a1eb
ckgathi <ckgathi@bhp.org.bw>
2014-04-09 13:04:47 +0200

    modify the producer factore

--- device/sync/tests/factories/producer_factory.py
+++ device/sync/tests/factories/producer_factory.py
@@ -5,6 +5,6 @@ from edc.device.sync.models import Producer
 class ProducerFactory(BaseUuidModelFactory):
     FACTORY_FOR = Producer
 
-    name = 'producer'
+    name = 'dispatch_destination'
     settings_key = 'dispatch_destination'
     url = 'http://'
