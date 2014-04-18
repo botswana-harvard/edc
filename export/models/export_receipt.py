aba62615fe2b9c7c60f887572ec79c531ab22307
erik van widenfelt <ew2789@gmail.com>
2014-04-18 10:18:40 +0200

    add admin for export receipts

--- export/models/export_receipt.py
+++ export/models/export_receipt.py
@@ -37,12 +37,6 @@ class ExportReceipt(BaseSyncUuidModel):
         # TODO: get this dashboard url
         return 'dashboard?'
 
-#     def render(self):
-#         url = reverse('view_transaction_url', kwargs={'app_label': self._meta.app_label, 'model_name': self._meta.object_name.lower(), 'pk': self.pk})
-#         ret = """<a href="{url}" class="add-another" id="add_id_report" onclick="return showAddAnotherPopup(this);"> <img src="/static/admin/img/icon_addlink.gif" width="10" height="10" alt="View transaction"/></a>""".format(url=url)
-#         return ret
-#     render.allow_tags = True
-
     class Meta:
         app_label = 'export'
         ordering = ('-timestamp', )
