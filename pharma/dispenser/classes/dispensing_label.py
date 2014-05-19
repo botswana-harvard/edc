06e9feb510b1a911f74b7d12cb4f79b93a720237
melissa kewagamang <mkewagamang@bhp.org.bw>
2014-05-19 13:38:18 +0200

    fix dispensing

--- pharma/dispenser/classes/dispensing_label.py
+++ pharma/dispenser/classes/dispensing_label.py
@@ -22,22 +22,23 @@ class DispensingLabel(ModelLabel):
             })
         self.label_context.update(**custom)
 
-    def get_template_prep(self):
+    def __init__(self):
+        super(DispensingLabel, self).__init__()
         label_template = 'dispensing'
-        try:
-            zpl_template = ZplTemplate.objects.get(name=label_template)
-        except:
-            zpl_template = ZplTemplate.objects.create(
-                name=label_template,
-                template=('^XA'
-                    '^FO100,25^A0N,25^FDBotswana-Harvard Partnership - SID ${sid}^FS'
-                    '^FO100,50^BY2.0^BCN,50,N,N,N'
-                    '^BY^FD${barcode_value}^FS'
-                    '^FO100,120^A0N,20^FD${barcode_value}^FS'
-                    '^FO100,150^A0N,30^FD${subject_identifier} [${initials}]^FS'
-                    '^FO100,180^A0N,30^FD${treatment}^FS'
-                    '^FO100,210^A0N,30^FD${dose}^FS'
-                    '^FO100,270^A0N,40^FD${packing_amount} ${packing_unit}^FS'
-                    '^FO100,330^A0N,30^FDdispensed on ${dispense_date} by ${user_created}^FS'
-                    '^XZ'))
-        return zpl_template
+        if not ZplTemplate.objects.get(name=label_template):
+            template_string = ('^XA'
+                        '^FO100,25^A0N,25^FDBotswana-Harvard Partnership - SID ${sid}^FS'
+                        '^FO100,50^BY2.0^BCN,50,N,N,N'
+                        '^BY^FD${barcode_value}^FS'
+                        '^FO100,120^A0N,20^FD${barcode_value}^FS'
+                        '^FO100,150^A0N,30^FD${subject_identifier} [${initials}]^FS'
+                        '^FO100,180^A0N,30^FD${treatment}^FS'
+                        '^FO100,210^A0N,30^FD${dose}^FS'
+                        '^FO100,270^A0N,40^FD${packing_amount} ${packing_unit}^FS'
+                        '^FO100,330^A0N,30^FDdispensed on ${dispense_date} by ${user_created}^FS'
+                        '^XZ')
+            self.zpl_template = ZplTemplate.objects.create(
+                        name=label_template,
+                        template=template_string)
+        else:
+            self.zpl_template = ZplTemplate.objects.get(name=label_template)
