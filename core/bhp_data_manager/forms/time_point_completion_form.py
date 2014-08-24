4a560b2f3380b3b2f8e24b037b4e39ea24680e8d
melissa <mrbkew@gmail.com>
2014-08-24 16:55:06 +0200

    form validations, method update, formfield_for_foreinkey update

--- core/bhp_data_manager/forms/time_point_completion_form.py
+++ core/bhp_data_manager/forms/time_point_completion_form.py
@@ -10,11 +10,18 @@ class TimePointCompletionForm(BaseModelForm):
         if cleaned_data.get('status') == 'feedback' and not cleaned_data.get('comment'):
             raise forms.ValidationError('If feedback is being given, please provide a fully detailed description in the comment box below')
 
-        if cleaned_data.get('subject_withdrew') == 'Yes' and not cleaned_data.get('reasons_withdrawn'):
-            raise forms.ValidationError('If subject is withdrawing, what are the reasons for withdrawal?')
+        if cleaned_data.get('subject_withdrew') == 'Yes' and cleaned_data.get('reasons_withdrawn') == 'N/A':
+            raise forms.ValidationError('If subject is withdrawing, REASON for withdrawal cannot be NOT APPLICABLE')
 
         if cleaned_data.get('subject_withdrew') == 'Yes' and not cleaned_data.get('date_withdrawn'):
             raise forms.ValidationError('If subject is withdrawing, please provide date of withdrawal')
+
+        if cleaned_data.get('subject_withdrew') == 'Yes' and cleaned_data.get('date_withdrawn'):
+            from apps.bcpp_subject.models import SubjectConsent
+            consent = SubjectConsent.objects.get(registered_subject__subject_identifier=cleaned_data.get('registered_subject').subject_identifier)
+            if consent:
+                if cleaned_data.get('date_withdrawn', None) != consent.consent_datetime:
+                    raise forms.ValidationError('the consent withdrawal date IS NOT EQUAL to the consent date. Got {0} != {1}'.format(cleaned_data.get('date_withdrawn'), consent.consent_datetime))
 
         return cleaned_data
 
