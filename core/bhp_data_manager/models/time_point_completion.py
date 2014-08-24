2b2187ab08e93f7a9670a7ad84e8f28b842a596f
erik van widenfelt <ew2789@gmail.com>
2014-08-24 21:02:08 +0200

    refactor timecompletion model.

--- core/bhp_data_manager/models/time_point_completion.py
+++ core/bhp_data_manager/models/time_point_completion.py
@@ -1,36 +1,28 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.core.exceptions import ImproperlyConfigured
-from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 from edc.subject.appointment.models import Appointment
 from edc.choices.common import YES_NO_NA
 from edc.core.crypto_fields.fields import EncryptedTextField
 from edc.audit.audit_trail import AuditTrail
-from edc.subject.registration.models import RegisteredSubject
-from edc.subject.visit_schedule.models import VisitDefinition
 from edc.base.model.models import BaseModel
 
 
 class TimePointCompletion(BaseModel):
     """ All completed appointments are noted in this form.
-        Only authorized users can access this form.
-        This form basically allows the user to definitely confirm
-        that the appointment has been completed"""
 
-    appointment = models.ForeignKey(Appointment, null=True)
-    date_added = models.DateTimeField(
-        verbose_name='Date added',
-        default=datetime.today(),
-        null=True)
-    registered_subject = models.ForeignKey(RegisteredSubject,
-        max_length=75)
-    the_visit_code = models.ForeignKey(VisitDefinition,
-        max_length=30,
-        help_text=('the subjects current completed visit')
-        )
+    Only authorized users can access this form. This form basically
+    allows the user to definitely confirm that the appointment has
+    been completed"""
+
+    appointment = models.ForeignKey(Appointment)
+
+    close_datetime = models.DateTimeField(
+        verbose_name='Date and time appointment "closed" for edit.',
+        default=datetime.today())
+
     status = models.CharField(
         max_length=35,
         choices=(
@@ -38,22 +30,15 @@ class TimePointCompletion(BaseModel):
             ('feedback', 'Feedback'),
             ('closed', 'Closed')),
         default='open',
-        null=True,
         help_text='')
+
     comment = EncryptedTextField(
         max_length=500,
         null=True,
         blank=True)
-    authorization = models.ForeignKey(User,
-        verbose_name='Authorization (signature)',
-        blank=True,
-        null=True,
-        editable=False
-        )
-
-    #adding fields for withdrawal of consent
 
     subject_withdrew = models.CharField(
+        verbose_name='Did the participant withdraw consent?',
         max_length=35,
         choices=YES_NO_NA,
         default='N/A',
@@ -61,6 +46,7 @@ class TimePointCompletion(BaseModel):
         help_text='Use ONLY when subject has changed mind and wishes to withdraw consent')
 
     reasons_withdrawn = models.CharField(
+        verbose_name='Reason participant withdrew consent',
         max_length=35,
         choices=(
             ('changed_mind', 'Subject changed mind'),
@@ -69,7 +55,8 @@ class TimePointCompletion(BaseModel):
         default='N/A',
         help_text='')
 
-    date_withdrawn = models.DateTimeField(
+    withdraw_datetime = models.DateTimeField(
+        verbose_name='Date and time participant withdrew consent',
         null=True,
         blank=True)
 
