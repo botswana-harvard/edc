1d0171363cb2a3d7d502b990c293f1ad8087f625
erik van widenfelt <ew2789@gmail.com>
2014-08-24 21:13:16 +0200

    refactor timecompletion model.

--- core/bhp_data_manager/models/time_point_completion.py
+++ core/bhp_data_manager/models/time_point_completion.py
@@ -13,8 +13,8 @@ from edc.base.model.models import BaseModel
 class TimePointCompletion(BaseModel):
     """ All completed appointments are noted in this form.
 
-    Only authorized users can access this form. This form basically
-    allows the user to definitely confirm that the appointment has
+    Only authorized users can access this form. This form allows
+    the user to definitely confirm that the appointment has
     been completed"""
 
     appointment = models.ForeignKey(Appointment)
@@ -24,7 +24,7 @@ class TimePointCompletion(BaseModel):
         default=datetime.today())
 
     status = models.CharField(
-        max_length=35,
+        max_length=15,
         choices=(
             ('open', 'Open'),
             ('feedback', 'Feedback'),
@@ -39,7 +39,7 @@ class TimePointCompletion(BaseModel):
 
     subject_withdrew = models.CharField(
         verbose_name='Did the participant withdraw consent?',
-        max_length=35,
+        max_length=15,
         choices=YES_NO_NA,
         default='N/A',
         null=True,
@@ -71,21 +71,20 @@ class TimePointCompletion(BaseModel):
         return self.date_added
 
     def save(self, *args, **kwargs):
-        self.confirm_appointment_status_before_setting_time_point_completion_to_closed()
+        self.confirm_before_closed()
         super(TimePointCompletion, self).save(*args, **kwargs)
 
-    def confirm_appointment_status_before_setting_time_point_completion_to_closed(self):
-        """closing off only appt that are either done/incomplete/cancelled ONLY"""
+    def confirm_before_closed(self, exception_cls):
+        """Closing off only appt that are either done/incomplete/cancelled ONLY."""
+        exception_cls = exception_cls or ValidationError
         if self.status == 'closed':
-            from edc.subject.appointment.models import Appointment
-            appointment = Appointment.objects.filter(registered_subject=self.registered_subject)
-            if appointment[0].appt_status == 'new':
-                raise ValidationError('You cannot close an appointment that has a NEW status')
-            if appointment[0].appt_status == 'in_progress':
-                raise ValidationError('You cannot close an appointment that is still in progress')
-        elif self.status[0] == 'closed':
-            if appointment[0].appt_status != ['done', 'incomplete', 'cancelled']:
-                raise ValidationError('You cannot make a close off when the appointment status is not equal to DONE or INCOMPLETE or CANCELLED. Check appointment status first')
+            if self.appointment.appt_status == 'new':
+                raise exception_cls('You cannot close an appointment that has a NEW status')
+            elif self.appointment.appt_status == 'in_progress':
+                raise exception_cls('You cannot close an appointment that is still in progress')
+        else:
+            if self.appointment.appt_status != ['done', 'incomplete', 'cancelled']:
+                raise exception_cls('You cannot make a close off when the appointment status is not equal to DONE or INCOMPLETE or CANCELLED. Check appointment status first')
 
     def dashboard(self):
         ret = None
