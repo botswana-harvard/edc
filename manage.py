0d5989f9d127a09b403144b4e64bbba551c2aba1
erik van widenfelt <ew2789@gmail.com>
2014-08-27 10:19:06 +0200

    update PATH in edc.manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
-SOURCE_DIR = os.path.expanduser('~/source')
+from unipath import Path
+
+SOURCE_ROOT = Path(os.path.dirname(os.path.realpath(__file__))).ancestor(2)  # e.g. /home/django/source
+EDC_DIR = SOURCE_ROOT.child('edc_project')  # e.g. /home/django/source/edc_project/
+LIS_DIR = SOURCE_ROOT.child('lis_project')  # e.g. /home/django/source/lis_project/
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-    sys.path.insert(0, os.path.join(SOURCE_DIR, 'edc_project/'))
-    sys.path.insert(1, os.path.join(SOURCE_DIR, 'lis_project/'))
+    sys.path.insert(0, os.path.join(EDC_DIR))
+    sys.path.insert(1, os.path.join(LIS_DIR))
 
     from django.core.management import execute_from_command_line
 
