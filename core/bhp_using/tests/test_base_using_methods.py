8fdcbd908e7a1eed6cf4cd1a984d3f3eb9781cc7
erik van widenfelt <ew2789@gmail.com>
2014-08-28 14:23:48 +0200

    update tests, confirm pass as TestCase

--- core/bhp_using/tests/test_base_using_methods.py
+++ core/bhp_using/tests/test_base_using_methods.py
@@ -7,27 +7,45 @@ from ..exceptions import UsingError, UsingSourceError, UsingDestinationError
 
 
 class TestBaseUsingMethods(TestCase):
+    using_source = 'default'
+    using_destination = 'destination'
 
     def test_methods(self):
-        using_source = 'default'
-        using_destination = 'destination'
         # Base tests
         self.assertTrue('DEVICE_ID' in dir(settings), 'Settings attribute DEVICE_ID not found')
-        # raise source and destination cannot be the same
-        self.assertRaises(UsingError, BaseUsing, using_source, using_source)
-        # assert if not provided, server_device_id defaults to 99
-        self.assertEqual(BaseUsing(using_source, using_destination).server_device_id, '99')
-        # source must be either server or default
-        self.assertRaises(UsingSourceError, BaseUsing, 'not_default', using_source)
-        # assert 'xdefault' is not accepted
-        self.assertRaises(ImproperlyConfigured, BaseUsing(using_source, using_destination).is_valid_using, 'xdefault', 'source')
-        # assert accepts server_device_id as None of using_source is default
-        self.assertRaises(UsingSourceError, BaseUsing, using_source, using_destination, server_device_id=None)
-        # id source is default, must be server = 99
-        self.assertRaises(UsingSourceError, BaseUsing, using_source, using_destination, server_device_id='22')
-        # assert server_device_id cannot be None if osource is not default
+
+    def test_methods1(self):
+        """Assert source and destination cannot be the same."""
+        self.assertRaises(UsingError, BaseUsing, self.using_source, self.using_source)
+
+    def test_methods2(self):
+        """Asserts  if not provided, server_device_id defaults to 99."""
+        self.assertEqual(BaseUsing(self.using_source, self.using_destination).server_device_id, '99')
+
+    def test_methods3(self):
+        """Asserts source must be either server or default."""
+        self.assertRaises(UsingSourceError, BaseUsing, 'not_default', self.using_source)
+
+    def test_methods4(self):
+        """Asserts  'xdefault' is not accepted."""
+        self.assertRaises(ImproperlyConfigured, BaseUsing(self.using_source, self.using_destination).is_valid_using, 'xdefault', 'source')
+
+    def test_methods5(self):
+        """Asserts accepts server_device_id as None if self.using_source is default."""
+        self.assertTrue(isinstance(BaseUsing(self.using_source, self.using_destination, server_device_id=None), BaseUsing))
+
+    def test_methods6(self):
+        """Asserts if source is default, must be server = 99."""
+        self.assertRaises(UsingSourceError, BaseUsing, self.using_source, self.using_destination, server_device_id='22')
+
+    def test_methods7(self):
+        """Asserts  server_device_id cannot be None if source is not default."""
         self.assertRaises(UsingSourceError, BaseUsing, 'destination', 'dispatch_destination', server_device_id=None)
-        # assert server_device_id cannot be None if osource is not default
-        self.assertEqual(BaseUsing('server', 'dispatch_destination', server_device_id='22').server_device_id, '22')
-        # assert destination cannot be server
-        self.assertRaises(UsingDestinationError, BaseUsing, using_source, 'server')
+
+    def test_methods8(self):
+        """Asserts  server_device_id cannot be None if source is not default."""
+        self.assertRaises(UsingSourceError, BaseUsing('server', 'dispatch_destination', server_device_id='22'))
+
+    def test_methods9(self):
+        """Asserts destination cannot be server."""
+        self.assertRaises(UsingDestinationError, BaseUsing, self.using_source, 'server')
--- entry_meta_data/tests/tests_entry_meta_data.py
+++ entry_meta_data/tests/tests_entry_meta_data.py
@@ -1,5 +1,4 @@
-from django.db import IntegrityError
-from django.test import TestCase, TransactionTestCase, SimpleTestCase
+from django.test import TestCase
 
 from edc.core.bhp_variables.models import StudySite
 from edc.entry_meta_data.models import ScheduledEntryMetaData, RequisitionMetaData
@@ -17,7 +16,7 @@ from edc.lab.lab_profile.classes import site_lab_profiles
 from edc.testing.tests.factories import TestVisitFactory
 
 
-class TestsEntryMetaData(TransactionTestCase):
+class TestsEntryMetaData(TestCase):
 
     app_label = 'testing'
     consent_catalogue_name = 'v1'
@@ -38,7 +37,8 @@ class TestsEntryMetaData(TransactionTestCase):
         self.visit_definition = VisitDefinition.objects.get(code='1000')
         self.test_consent = TestConsentWithMixinFactory(gender='M', study_site=self.study_site)
         self.registered_subject = RegisteredSubject.objects.get(subject_identifier=self.test_consent.subject_identifier)
-        self.appointment = Appointment.objects.get(registered_subject=self.registered_subject)
+        self.appointment_count = VisitDefinition.objects.all().count()
+        self.appointment = Appointment.objects.get(registered_subject=self.registered_subject, visit_definition__code='1000')
 
     def test_creates_meta_data1(self):
         """No meta data if visit tracking form is not entered."""
@@ -118,17 +118,19 @@ class TestsEntryMetaData(TransactionTestCase):
         aliquot_type = TestAliquotType.objects.get(alpha_code=requisition_panel.aliquot_type_alpha_code)
         obj = TestRequisitionFactory(site=self.study_site, test_visit=self.test_visit, panel=panel, aliquot_type=aliquot_type, requisition_identifier='WJ0000003')
         obj.save()
-        self.assertEqual(RequisitionMetaData.objects.filter(entry_status='KEYED',
-                                                               registered_subject=self.registered_subject,
-                                                               lab_entry__app_label='testing',
-                                                               lab_entry__model_name='testrequisition',
-                                                               lab_entry__requisition_panel__name=panel.name).count(), 1)
+        self.assertEqual(RequisitionMetaData.objects.filter(
+            entry_status='KEYED',
+            registered_subject=self.registered_subject,
+            lab_entry__app_label='testing',
+            lab_entry__model_name='testrequisition',
+            lab_entry__requisition_panel__name=panel.name).count(), 1)
         obj.delete()
-        self.assertEqual(RequisitionMetaData.objects.filter(entry_status='NEW',
-                                                               registered_subject=self.registered_subject,
-                                                               lab_entry__app_label='testing',
-                                                               lab_entry__model_name='testrequisition',
-                                                               lab_entry__requisition_panel__name=panel.name).count(), 1)
+        self.assertEqual(RequisitionMetaData.objects.filter(
+            entry_status='NEW',
+            registered_subject=self.registered_subject,
+            lab_entry__app_label='testing',
+            lab_entry__model_name='testrequisition',
+            lab_entry__requisition_panel__name=panel.name).count(), 1)
 
     def test_creates_meta_data2(self):
         """Meta data is created when visit tracking form is added, each instance set to NEW."""
