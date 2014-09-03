89fde6d9bc242a5d77e13afbcd0d80de2aca69b2
erik van widenfelt <ew2789@gmail.com>
2014-09-03 02:01:07 +0200

    clean up signals, pep8

--- export/models/signals.py
+++ export/models/signals.py
@@ -5,18 +5,23 @@ from django.dispatch import receiver
 
 @receiver(post_save, weak=False, dispatch_uid="export_to_transaction_on_post_save")
 def export_to_transaction_on_post_save(sender, instance, raw, created, using, update_fields, **kwargs):
-    from ..managers import ExportHistoryManager  # causes import error if outside
-    for manager in sender._meta.concrete_managers:
-        if isinstance(manager[2], ExportHistoryManager):
-            change_type = 'I'
-            if not created:
-                change_type = 'U'
+    """Serializes the model instance to export history model
+    if manager exists."""
+    if not raw:
+        change_type = 'I'
+        if not created:
+            change_type = 'U'
+        try:
             sender.export_history.serialize_to_export_transaction(instance, change_type, using=using)
+        except AttributeError:
+            pass
 
 
 @receiver(pre_delete, weak=False, dispatch_uid="export_to_transaction_on_pre_delete")
 def export_to_transaction_on_pre_delete(sender, instance, using, **kwargs):
-    from ..managers import ExportHistoryManager  # causes import error if outside
-    for manager in sender._meta.concrete_managers:
-        if isinstance(manager[2], ExportHistoryManager):
-            sender.export_history.serialize_to_export_transaction(instance, 'D', using=using)
+    """Serializes the model instance, before deleting, to export
+    history model if manager exists."""
+    try:
+        sender.export_history.serialize_to_export_transaction(instance, 'D', using=using)
+    except AttributeError:
+        pass
