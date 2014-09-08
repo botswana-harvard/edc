4256fbcae4a88e484fb5b56b8fe0d35c623671ec
erik van widenfelt <ew2789@gmail.com>
2014-09-08 07:30:44 +0200

    add raw conditional

--- subject/visit_tracking/models/signals.py
+++ subject/visit_tracking/models/signals.py
@@ -5,9 +5,10 @@ from .base_visit_tracking import BaseVisitTracking
 
 
 @receiver(post_save, weak=False, dispatch_uid="base_visit_tracking_check_in_progress_on_post_save")
-def base_visit_tracking_check_in_progress_on_post_save(sender, instance, **kwargs):
+def base_visit_tracking_check_in_progress_on_post_save(sender, instance, raw, created, using, **kwargs):
     """Calls post_save method on the visit tracking instance."""
-    if isinstance(instance, BaseVisitTracking):
-        # TODO: how does this work with the scheduled_entry_meta_data_helper and show_scheduled_entries()
-        #       which also manipulates appointment in_progress?
-        instance.post_save_check_in_progress()
+    if not raw:
+        if isinstance(instance, BaseVisitTracking):
+            # TODO: how does this work with the scheduled_entry_meta_data_helper and show_scheduled_entries()
+            #       which also manipulates appointment in_progress?
+            instance.post_save_check_in_progress()
