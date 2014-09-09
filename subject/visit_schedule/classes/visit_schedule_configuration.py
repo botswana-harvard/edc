5505af6728d8991497283876236362278ea33914
erik van widenfelt <ew2789@gmail.com>
2014-09-10 01:32:44 +0200

    fix study_site_setup and site_mapper, use try/except get/create pattern, upfdate_fields, create instance

--- subject/visit_schedule/classes/visit_schedule_configuration.py
+++ subject/visit_schedule/classes/visit_schedule_configuration.py
@@ -7,7 +7,9 @@ from edc.core.bhp_content_type_map.models import ContentTypeMap
 
 EntryTuple = namedtuple('EntryTuple', 'order app_label model_name default_entry_status additional')
 MembershipFormTuple = namedtuple('MembershipFormTuple', 'name model visible')
-RequisitionPanelTuple = namedtuple('RequisitionPanelTuple', 'entry_order app_label model_name requisition_panel_name panel_type aliquot_type_alpha_code default_entry_status additional')
+RequisitionPanelTuple = namedtuple('RequisitionPanelTuple', 'entry_order app_label model_name '
+                                   'requisition_panel_name panel_type aliquot_type_alpha_code '
+                                   'default_entry_status additional')
 ScheduleGroupTuple = namedtuple('ScheduleTuple', 'name membership_form_name grouping_key comment')
 
 
@@ -21,9 +23,6 @@ class VisitScheduleConfiguration(object):
     membership_forms = OrderedDict()
     schedule_groups = OrderedDict()
     visit_definitions = OrderedDict()
-
-    def __init__(self):
-        self.verify()
 
     def __repr__(self):
         return '{0}.{1}'.format(self.app_label, self.name)
@@ -54,28 +53,32 @@ class VisitScheduleConfiguration(object):
             if schedule_group_name in self.membership_forms:
                 raise ImproperlyConfigured('Visit Schedule Configuration attribute schedule_groups '
                                            'cannot have the same name as a membership_form. '
-                                           'Got {0} in {1}.'.format(schedule_group_name, self.membership_forms.keys()))
+                                           'Got {0} in {1}.'.format(schedule_group_name,
+                                                                    self.membership_forms.keys()))
         for schedule_group in self.schedule_groups.itervalues():
-            if not schedule_group.membership_form_name in self.membership_forms:
+            if schedule_group.membership_form_name not in self.membership_forms:
                 raise ImproperlyConfigured('Visit Schedule Configuration attribute schedule_groups '
                                            'refers to a membership_form not listed in attribute membership_forms. '
-                                           'Got {0} not in {1}.'.format(schedule_group.membership_form_name, self.membership_forms.keys()))
+                                           'Got {0} not in {1}.'.format(schedule_group.membership_form_name,
+                                                                        self.membership_forms.keys()))
         for visit_definition in self.visit_definitions.itervalues():
             for entry in visit_definition.get('entries'):
                 if not get_model(entry.app_label, entry.model_name):
                     raise ImproperlyConfigured('Visit Schedule Configuration attribute entries refers '
-                                               'to model {0}.{1} which does not exist.'.format(entry.app_label, entry.model_name))
-        for visit_definition in self.visit_definitions.itervalues():
+                                               'to model {0}.{1} which does not exist.'.format(
+                                                   entry.app_label, entry.model_name))
             for requisition_item in visit_definition.get('requisitions'):
                 if not get_model(requisition_item.app_label, requisition_item.model_name):
                     raise ImproperlyConfigured('Visit Schedule Configuration attribute requisitions '
-                                               'refers to model {0}.{1} which does not exist.'.format(requisition_item.app_label, requisition_item.model_name))
+                                               'refers to model {0}.{1} which does not exist.'.format(
+                                                   requisition_item.app_label, requisition_item.model_name))
 
     def sync_content_type_map(self):
-        from edc.core.bhp_content_type_map.classes import ContentTypeMapHelper
-        content_type_map_helper = ContentTypeMapHelper()
-        content_type_map_helper.populate()
-        content_type_map_helper.sync()
+        pass
+#         from edc.core.bhp_content_type_map.classes import ContentTypeMapHelper
+#         content_type_map_helper = ContentTypeMapHelper()
+#         content_type_map_helper.populate()
+#         content_type_map_helper.sync()
 
     def rebuild(self):
         """Rebuild, WARNING which DELETES meta data."""
@@ -83,7 +86,8 @@ class VisitScheduleConfiguration(object):
         from edc.subject.entry.models import Entry
         from edc.subject.appointment.models import Appointment
 
-        self.sync_content_type_map()  # This will be required to be properly synced when creating entries.
+        # self.sync_content_type_map()  # This will be required to be properly synced when creating entries.
+        self.verify()
 
         for code in self.visit_definitions.iterkeys():
             if VisitDefinition.objects.filter(code=code):
@@ -101,7 +105,8 @@ class VisitScheduleConfiguration(object):
         """Builds and / or updates the visit schedule models."""
         from ..models import MembershipForm, ScheduleGroup, VisitDefinition
         from edc.subject.entry.models import Entry, LabEntry, RequisitionPanel
-        self.sync_content_type_map()  # This will be required to be properly synced when creating entries.
+        # self.sync_content_type_map()  # This will be required to be properly synced when creating entries.
+        self.verify()
         for membership_form in self.membership_forms.itervalues():
             if not MembershipForm.objects.filter(category=membership_form.name):
                 MembershipForm.objects.create(
@@ -215,7 +220,7 @@ class VisitScheduleConfiguration(object):
                         visit_definition=visit_definition_instance
                         ).update(entry_order=requisition_item.entry_order,
                                  default_entry_status=requisition_item.default_entry_status,
-                                  additional=requisition_item.additional)
+                                 additional=requisition_item.additional)
             for lab_entry in LabEntry.objects.filter(visit_definition=visit_definition_instance):
                 if (lab_entry.app_label, lab_entry.model_name) not in [(item.app_label, item.model_name) for item in visit_definition.get('requisitions')]:
                     lab_entry.delete()
