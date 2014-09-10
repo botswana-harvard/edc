4b9b279183dad667397730665ff0bb0e88c1a9ba
erik van widenfelt <ew2789@gmail.com>
2014-09-11 01:46:48 +0200

    refactor populate and sync

--- core/bhp_content_type_map/classes/content_type_map_helper.py
+++ core/bhp_content_type_map/classes/content_type_map_helper.py
@@ -12,7 +12,7 @@ class ContentTypeMapHelper(object):
         self.using = using or 'default'
 
     def populate(self):
-        """Populates ContentTypeMap with django's ContentTypecontent information."""
+        """Populates ContentTypeMap with django's ContentType information."""
         content_types = ContentType.objects.using(self.using).all()
         for content_type in content_types:
             create = True
@@ -25,39 +25,18 @@ class ContentTypeMapHelper(object):
                 ContentTypeMap.objects.filter(content_type=content_type).delete()
             finally:
                 if create:
-                    verbose_name = content_type.model_class()._meta.verbose_name
-                    # print 'Creating ContentTypeMap: {0}'.format(verbose_name)
-                    ContentTypeMap.objects.using(self.using).create(
-                        content_type=content_type,
-                        app_label=content_type.app_label,
-                        name=verbose_name,
-                        model=content_type.model,
-                        module_name=content_type.model)
-
-
-#             if not self.content_type_map_model_cls.objects.filter(content_type=content_type):
-#                 if not self.content_type_map_model_cls.objects.using(self.using).filter(content_type__name=content_type.name).count() == 1:
-#                     self.content_type_map_model_cls.objects.using(self.using).filter(content_type__name=content_type.name).delete()
-#                 if get_model(content_type.app_label, content_type.model):
-#                     verbose_name = get_model(content_type.app_label, content_type.model)._meta.verbose_name
-#                 else:
-#                     verbose_name = content_type.name
-#                 try:
-#                     self.content_type_map_model_cls.objects.using(self.using).create(
-#                         content_type=content_type,
-#                         app_label=content_type.app_label,
-#                         name=verbose_name,
-#                         model=content_type.model,
-#                         module_name=content_type.model)
-#                 except:
-#                     pass
-#        # make sure content_type_map_model_cls has as many records as djangos
-#        django_count = ContentType.objects.using(self.using).all().aggregate(Max('id'))
-#        bhp_count = self.content_type_map_model_cls.objects.using(self.using).all().aggregate(Max('id'))
-#        creation_count = django_count.get('id__max') - bhp_count.get('id__max')
-#        if creation_count > 0:
-#            for i in range(0, creation_count):
-#                self.content_type_map_model_cls.objects.create()
+                    try:
+                        verbose_name = content_type.model_class()._meta.verbose_name
+                        # print 'Creating ContentTypeMap: {0}'.format(verbose_name)
+                        ContentTypeMap.objects.using(self.using).create(
+                            content_type=content_type,
+                            app_label=content_type.app_label,
+                            name=verbose_name,
+                            model=content_type.model,
+                            module_name=content_type.model)
+                    except AttributeError as attribute_error:
+                        if 'object has no attribute \'_meta\'' in str(attribute_error):
+                            pass
 
     def sync(self):
         """Syncs content type map foreignkey with django's ContentType id.
@@ -75,5 +54,6 @@ class ContentTypeMapHelper(object):
                 except ContentType.DoesNotExist:
                     pass
             except ContentTypeMapError:
-                print 'Deleting stale ContentTypeMap {} {}'.format(content_type_map.app_label, content_type_map.model)
+                print 'Deleting stale ContentTypeMap {} {}'.format(content_type_map.app_label,
+                                                                   content_type_map.model)
                 content_type_map.delete()
