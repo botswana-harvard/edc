f7c89dca25c9eee33785b3b88d048daf4a7e233a
erik van widenfelt <ew2789@gmail.com>
2014-09-11 01:46:19 +0200

    add search list

--- audit/audit_trail.py
+++ audit/audit_trail.py
@@ -27,14 +27,22 @@ class BaseAuditModelAdmin(admin.ModelAdmin):
     def __init__(self, *args, **kwargs):
         model_cls = args[0]
         super(BaseAuditModelAdmin, self).__init__(*args, **kwargs)
-        if 'visit_model' in dir(model_cls):
-            self.search_fields = ('{0}__appointment__registered_subject__subject_identifier'.format(convert_from_camel(model_cls._meta.object_name)), 'id', )
+        self.search_fields = ['_audit_subject_identifier', 'id', 'revision']
+        if 'registered_subject' in dir(model_cls):
+            self.search_fields = ['registered_subject__subject_identifier'] + self.search_fields
+        elif 'appointment' in dir(model_cls):
+            self.search_fields = ['appointment__registered_subject__subject_identifier'] + self.search_fields
+        elif 'visit_model' in dir(model_cls):
+            self.search_fields = ['{0}__appointment__registered_subject__subject_identifier'.format(
+                                  convert_from_camel(model_cls._meta.object_name).split('_audit')[0])] + self.search_fields
         self.readonly_fields = [field.name for field in model_cls._meta.fields]
 
     date_hierarchy = '_audit_timestamp'
-#     search_fields = ('_audit_subject_identifier', 'id')
-    list_display = ('_audit_id', '_audit_change_type', '_audit_timestamp', 'created', 'modified', 'user_created', 'user_modified', 'hostname_created', 'hostname_modified')
-    list_filter = ('_audit_change_type', '_audit_timestamp', 'created', 'modified', 'user_created', 'user_modified', 'hostname_created', 'hostname_modified')
+    list_display = ('_audit_id', '_audit_subject_identifier', '_audit_change_type', '_audit_timestamp',
+                    'created', 'modified', 'user_created', 'user_modified', 'hostname_created',
+                    'hostname_modified')
+    list_filter = ('_audit_change_type', '_audit_timestamp', 'created', 'modified', 'user_created',
+                   'user_modified', 'hostname_created', 'hostname_modified')
 
 
 class AuditTrail(object):
