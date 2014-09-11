6e9bc433c4c55642646b5165557cedd725cdbf0e
SirOne Pharatlhatlhe <sirone@One.local>
2014-09-11 13:40:53 +0200

    fixing visit_schedule by adding _

--- subject/visit_schedule/classes/visit_schedule_configuration.py
+++ subject/visit_schedule/classes/visit_schedule_configuration.py
@@ -90,9 +90,9 @@ class VisitScheduleConfiguration(object):
         """Rebuild, WARNING which DELETES meta data."""
         Appointment = get_model('appointment', 'appointment')
         Entry = get_model('entry', 'entry')
-        MembershipForm = get_model('visitschedule', 'membershipform')
-        VisitDefinition = get_model('visitschedule', 'visitdefinition')
-        ScheduleGroup = get_model('visitschedule', 'schedulegroup')
+        MembershipForm = get_model('visit_schedule', 'membershipform')
+        VisitDefinition = get_model('visit_schedule', 'visitdefinition')
+        ScheduleGroup = get_model('visit_schedule', 'schedulegroup')
         self.verify()
         for code in self.visit_definitions.iterkeys():
             if VisitDefinition.objects.filter(code=code):
@@ -111,9 +111,9 @@ class VisitScheduleConfiguration(object):
         Entry = get_model('entry', 'entry')
         RequisitionPanel = get_model('entry', 'requisitionpanel')
         LabEntry = get_model('entry', 'labentry')
-        MembershipForm = get_model('visitschedule', 'membershipform')
-        VisitDefinition = get_model('visitschedule', 'visitdefinition')
-        ScheduleGroup = get_model('visitschedule', 'schedulegroup')
+        MembershipForm = get_model('visit_schedule', 'membershipform')
+        VisitDefinition = get_model('visit_schedule', 'visitdefinition')
+        ScheduleGroup = get_model('visit_schedule', 'schedulegroup')
         self.verify()
         while True:
             try:
