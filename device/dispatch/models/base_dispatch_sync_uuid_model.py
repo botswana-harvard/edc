d8495f3a9dca755bb6a3383c808e27c94b006e4f
ckgathi <ckgathi@bhp.org.bw>
2014-09-22 12:54:48 +0200

    add update fields to the signature of the method bypass_for_edit_dispatched_as_item and _bypass_for_edit

--- device/dispatch/models/base_dispatch_sync_uuid_model.py
+++ device/dispatch/models/base_dispatch_sync_uuid_model.py
@@ -217,7 +217,7 @@ class BaseDispatchSyncUuidModel(BaseSyncUuidModel):
                         household_structure
                             household.household_identifier
 
-            where 'household' is the user container with
+            where 'household' is the user container with 
             identifier attr 'household_identifier',
             <self> would return something like this:
                 (Household, 'household_structure_member__household_structure__household__household_identifier')
@@ -225,9 +225,9 @@ class BaseDispatchSyncUuidModel(BaseSyncUuidModel):
         raise ImproperlyConfigured('Model {0} is not configured for dispatch. '
                                    'Missing method \'dispatch_container_lookup\''.format(self._meta.object_name))
 
-    def _bypass_for_edit(self, using=None):
+    def _bypass_for_edit(self, using=None, update_fields=None):
         using = using or 'default'
-        if self.bypass_for_edit_dispatched_as_item(using):
+        if not self.bypass_for_edit_dispatched_as_item(using, update_fields):
             if not self.id:
                 raise AlreadyDispatchedItem('Model {0}-{1}, although dispatched, may only be '
                                             'conditionally edited. New instances are not '
@@ -235,10 +235,8 @@ class BaseDispatchSyncUuidModel(BaseSyncUuidModel):
             return True
         return True
 
-    def bypass_for_edit_dispatched_as_item(self, using=None):
-        """Users may override to allow a model to be edited even though it is dispatched.
-
-        To block editing dispatched items, raise an AlreadyDispatchedItem exception.
+    def bypass_for_edit_dispatched_as_item(self, using=None, update_fields=None):
+        """Users may override to allow a model to be edited even thoug it is dispatched.
 
         .. warning:: avoid using this. it only allows edits. you are responsible to
                   ensure your actions will not lead to data conflicts. so it is best to also
@@ -250,7 +248,7 @@ class BaseDispatchSyncUuidModel(BaseSyncUuidModel):
         """Returns the models 'dispatched' status in model DispatchItemRegister."""
         is_dispatched = False
         if self.is_dispatchable_model():
-            # if self.id:
+            #if self.id:
             if self.dispatched_item(using):
                 is_dispatched = True
             if not is_dispatched:
@@ -292,15 +290,16 @@ class BaseDispatchSyncUuidModel(BaseSyncUuidModel):
 
     def save(self, *args, **kwargs):
         using = kwargs.get('using')
+        update_fields = kwargs.get('update_fields') or None
         if self.id:
             if self.is_dispatchable_model():
                 if self.is_dispatch_container_model():
-                    if not self._bypass_for_edit(using):
+                    if not self._bypass_for_edit(using, update_fields):
                         if self.is_dispatched_as_container(using):
                             raise AlreadyDispatchedContainer('Model {0}-{1} is currently dispatched '
                                                              'as a container for other dispatched '
                                                              'items.'.format(self._meta.object_name, self.pk))
-                if not self._bypass_for_edit(using):
+                if not self._bypass_for_edit(using, update_fields):
                     if self.is_dispatched_as_item(using):
                         raise AlreadyDispatchedItem('Model {0}-{1} is currently dispatched'.format(self._meta.object_name,
                                                                                                    self.pk))
