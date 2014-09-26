1aa33aafadcc424d7584d85fe2368d27f80bbdbc
erik van widenfelt <ew2789@gmail.com>
2014-09-26 12:09:41 +0200

    add is_active action for producer, ip to list_display

--- device/sync/admin/producer_admin.py
+++ device/sync/admin/producer_admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from edc.base.modeladmin.admin import BaseModelAdmin
 from ..models import Producer
-from ..actions import reset_producer_status
+from ..actions import reset_producer_status, toggle_producer_is_active
 
 
 class ProducerAdmin(BaseModelAdmin):
 
-    list_display = ('name', 'url', 'is_active', 'sync_datetime', 'sync_status', 'comment')
+    list_display = ('name', 'url', 'producer_ip', 'is_active', 'sync_datetime', 'sync_status', 'comment')
     list_filter = ('is_active', 'sync_datetime', 'sync_status',)
 
-    actions = [reset_producer_status]
+    actions = [reset_producer_status, toggle_producer_is_active]
 
 admin.site.register(Producer, ProducerAdmin)
