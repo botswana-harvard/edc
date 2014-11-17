4865c960a530e7a5eb4e927329e626d3c0c24b46
erik van widenfelt <ew2789@gmail.com>
2014-11-17 08:44:55 +0200

    pep8, change tp try/except instead of using filter

--- subject/visit_tracking/admin/base_visit_tracking_model_admin.py
+++ subject/visit_tracking/admin/base_visit_tracking_model_admin.py
@@ -21,12 +21,17 @@ class BaseVisitTrackingModelAdmin(BaseModelAdmin):
     def __init__(self, *args, **kwargs):
         super(BaseVisitTrackingModelAdmin, self).__init__(*args, **kwargs)
         if not self.visit_model:
-            raise ImproperlyConfigured("Class attribute \'visit model\' on BaseVisitModelAdmin for model {0} may not be None. Please correct.".format(self.model))
+            raise ImproperlyConfigured('Class attribute \'visit model\' on BaseVisitModelAdmin '
+                                       'for model {0} may not be None. Please correct.'.format(self.model))
         if not self.visit_model_foreign_key:
             # TODO: rather remove this, user needs to specify this as a class attribute, no need to help out
-            self.visit_model_foreign_key = [fk for fk in [f for f in self.model._meta.fields if isinstance(f, ForeignKey)] if fk.rel.to._meta.module_name == self.visit_model._meta.module_name]
+            self.visit_model_foreign_key = [
+                fk for fk in [f for f in self.model._meta.fields if isinstance(f, ForeignKey)] \
+                if fk.rel.to._meta.module_name == self.visit_model._meta.module_name]
             if not self.visit_model_foreign_key:
-                raise ValueError("The model for {0} requires a foreign key to visit model {1}. None found. Either correct the model or change the ModelAdmin class.".format(self, self.visit_model))
+                raise ValueError('The model for {0} requires a foreign key to visit model {1}. '
+                                 'None found. Either correct the model or change the ModelAdmin '
+                                 'class.'.format(self, self.visit_model))
             else:
                 self.visit_model_foreign_key = self.visit_model_foreign_key[0].name
         self.extend_list_display()
@@ -35,7 +40,9 @@ class BaseVisitTrackingModelAdmin(BaseModelAdmin):
 
     def extend_search_fields(self):
         self.search_fields = list(self.search_fields)
-        for item in ['id', self.visit_model_foreign_key + '__appointment__registered_subject__subject_identifier', self.visit_model_foreign_key + '__pk']:
+        for item in ['id',
+                     '{}__appointment__registered_subject__subject_identifier'.format(self.visit_model_foreign_key),
+                     '{}__pk'.format(self.visit_model_foreign_key)]:
             if item not in self.search_fields:
                 self.search_fields.append(item)
         self.search_fields = tuple(self.search_fields)
@@ -74,7 +81,8 @@ class BaseVisitTrackingModelAdmin(BaseModelAdmin):
                 export_as_csv_action(
                     exclude=['id', self.visit_model_foreign_key],
                     extra_fields=OrderedDict(
-                        {'subject_identifier': self.visit_model_foreign_key + '__appointment__registered_subject__subject_identifier',
+                        {'subject_identifier': ('{}__appointment__registered_subject'
+                                                '__subject_identifier').format(self.visit_model_foreign_key),
                          'visit_report_datetime': '%s__report_datetime' % self.visit_model_foreign_key,
                          'gender': self.visit_model_foreign_key + '__appointment__registered_subject__gender',
                          'dob': self.visit_model_foreign_key + '__appointment__registered_subject__dob',
@@ -83,8 +91,8 @@ class BaseVisitTrackingModelAdmin(BaseModelAdmin):
                          'visit': self.visit_model_foreign_key + '__appointment__visit_definition__code',
                          'visit_instance': self.visit_model_foreign_key + '__appointment__visit_instance'}),
                     ),
-                    'export_as_csv_action',
-                    "Export to CSV with visit and demographics")
+                'export_as_csv_action',
+                'Export to CSV with visit and demographics')
         return actions
 
     def add_view(self, request, form_url='', extra_context=None):
@@ -93,7 +101,8 @@ class BaseVisitTrackingModelAdmin(BaseModelAdmin):
         To be used by supplemental fields, etc."""
         self.visit_model_attr = request.GET.get('visit_attr')
         self.visit_model_pk = request.GET.get(self.visit_model_attr)
-        return super(BaseVisitTrackingModelAdmin, self).add_view(request, form_url=form_url, extra_context=extra_context)
+        return super(BaseVisitTrackingModelAdmin, self).add_view(
+            request, form_url=form_url, extra_context=extra_context)
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         """Sets the values for the visit model object name and the visit model pk.
@@ -101,12 +110,13 @@ class BaseVisitTrackingModelAdmin(BaseModelAdmin):
         To be used by supplemental fields, etc."""
         self.visit_model_attr = request.GET.get('visit_attr')
         self.visit_model_pk = request.GET.get(self.visit_model_attr)
-        return super(BaseVisitTrackingModelAdmin, self).change_view(request, object_id, form_url=form_url, extra_context=extra_context)
+        return super(BaseVisitTrackingModelAdmin, self).change_view(
+            request, object_id, form_url=form_url, extra_context=extra_context)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         visit_model_helper = VisitModelHelper()
         if db_field.name == visit_model_helper.get_visit_field(model=self.model, visit_model=self.visit_model):
-            #if not request.GET.get('subject_identifier', None):
+            # if not request.GET.get('subject_identifier', None):
             #    raise TypeError('Subject identifier cannot be none when accessing {0}.'.format(db_field.name))
             kwargs["queryset"] = visit_model_helper.set_visit_queryset(
                 visit_model=self.visit_model,
