dfb38aab2e0fe6615bd3b1b0d231ffcf445f2e8d
erik van widenfelt <ew2789@gmail.com>
2015-03-07 09:47:54 +0200

    separate classes between objects and models

---
+++ export/classes/export_object_as_csv.py
@@ -0,0 +1,10 @@
+from .base_export_object import BaseExportObject
+
+
+class ExportObjectAsCsv(BaseExportObject):
+    """Exports an object to CSV.
+
+    See bccp_export.helpers for examples of exportable objects."""
+
+    def update_export_transaction(self, *args):
+        pass
