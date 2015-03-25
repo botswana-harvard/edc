8c1c513fe6565d7055abf01a1fb1c05ced4f713b
erik van widenfelt <ew2789@gmail.com>
2015-03-25 14:45:49 +0200

    fix to handle passed as dictionary.

--- export/helpers/export_model_helper.py
+++ export/helpers/export_model_helper.py
@@ -14,7 +14,8 @@ class ExportModelHelper(ExportHelper):
     def __init__(self, export_plan, app_label, model_name, exception_cls=None, notify=None):
         self.app_label = app_label
         self.model_name = model_name
-        super(ExportModelHelper, self).__init__(export_plan, exception_cls=exception_cls, notify=notify)
+        super(ExportModelHelper, self).__init__(
+            export_plan, exception_cls=exception_cls, notify=notify)
 
     def reset(self):
         """Resets instance attr."""
@@ -40,10 +41,11 @@ class ExportModelHelper(ExportHelper):
             self.export_transactions = ExportTransaction.objects.filter(
                 app_label=self.model._meta.app_label,
                 object_name=self.model._meta.object_name,
-                ).exclude(status__in=[CLOSED, 'cancelled'], )  # if already exported but not closed, will be sent again
+                ).exclude(status__in=[CLOSED, 'cancelled'], )  # if already exported but not closed, will send again
             # decrypt export transactions and add to a list
             for export_transaction in self.export_transactions:
-                for obj in serializers.deserialize("json", FieldCryptor('aes', 'local').decrypt(export_transaction.tx)):
+                for obj in serializers.deserialize(
+                        "json", FieldCryptor('aes', 'local').decrypt(export_transaction.tx)):
                     obj.object.export_transaction = export_transaction
                     self._transactions.append(obj.object)
         return self._transactions
