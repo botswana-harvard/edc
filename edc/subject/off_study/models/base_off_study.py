16f9e103e69592f38d4cd4d67377e7dd1846933f
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2015-09-18 09:38:39 +0200

    putting back registered_subject to BaseOffStudy

--- edc/subject/off_study/models/base_off_study.py
+++ edc/subject/off_study/models/base_off_study.py
@@ -4,7 +4,8 @@ from django.core.exceptions import ImproperlyConfigured
 from django.db import models
 from django.db.models import Max, get_app, get_models
 
-from edc.base.model.fields import OtherCharField
+from edc_base.model.fields import OtherCharField
+from edc.subject.registration.models.registered_subject import RegisteredSubject
 from edc.choices.common import YES_NO
 from edc.constants import YES, NO
 from edc.core.crypto_fields.utils import mask_encrypted
@@ -16,6 +17,8 @@ from ..managers import OffStudyManager
 
 class BaseOffStudy(models.Model):
     """Base model for the Off Study model in an app."""
+    registered_subject = models.OneToOneField(RegisteredSubject)
+
     offstudy_date = models.DateField(
         verbose_name="Off-study Date",
         help_text="")
