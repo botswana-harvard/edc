ce0e9ddac60a8a958f4062aa0a6f261d188fac23
erikvw <ew2789@gmail.com>
2015-09-24 13:42:19 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='edc',
-    version='1.13.9',
+    version='1.13.10',
     author=u'Botswana-Harvard AIDS Institute',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
