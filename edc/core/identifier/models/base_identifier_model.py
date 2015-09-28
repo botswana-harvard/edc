b5531a23c23040183a436526cd1efa4a2b967fa9
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2015-09-28 10:09:01 +0200

    base_identifier subclass BaseSyncUuidModel

--- edc/core/identifier/models/base_identifier_model.py
+++ edc/core/identifier/models/base_identifier_model.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-from edc_base.model.models import BaseModel
+from edc.device.sync.models import BaseSyncUuidModel
 
 from ..managers import IdentifierManager
 
 
-class BaseIdentifierModel(BaseModel):
+class BaseIdentifierModel(BaseSyncUuidModel):
     """Store identifiers as allocated."""
 
     identifier = models.CharField(max_length=36, unique=True, editable=False)
