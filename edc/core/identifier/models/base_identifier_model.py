092f574abc1e5cced16e65bbd8772971f8fb9692
erikvw <ew2789@gmail.com>
2015-09-28 18:59:30 +0200

    fix overlapping inheritance

--- edc/core/identifier/models/base_identifier_model.py
+++ edc/core/identifier/models/base_identifier_model.py
@@ -2,12 +2,10 @@ from django.db import models
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-from edc.device.sync.models import BaseSyncUuidModel
-
 from ..managers import IdentifierManager
 
 
-class BaseIdentifierModel(BaseSyncUuidModel):
+class BaseIdentifierModel(models.Model):
     """Store identifiers as allocated."""
 
     identifier = models.CharField(max_length=36, unique=True, editable=False)
