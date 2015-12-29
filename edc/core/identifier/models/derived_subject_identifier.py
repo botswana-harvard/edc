a57899778a46d826c1635713e15b026ca7be31b9
erikvw <ew2789@gmail.com>
2015-12-29 11:27:37 +0200

    revert edc.core.identifier for now because of lab (but use edc-identifier)

---
+++ edc/core/identifier/models/derived_subject_identifier.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from edc_base.model.models import BaseModel
+
+
+class DerivedSubjectIdentifier(BaseModel):
+    """Store subject identifiers derived from another participant, e.g. infant identifier from maternal identifier"""
+
+    subject_identifier = models.CharField(
+        max_length=25,
+        unique=True)
+
+    base_identifier = models.CharField(
+        max_length=25)
+
+    padding = models.IntegerField(null=True)
+
+    def __unicode__(self):
+        return self.subject_identifier
+
+    class Meta:
+        app_label = 'identifier'
+        db_table = 'bhp_identifier_derivedsubjectidentifier'
