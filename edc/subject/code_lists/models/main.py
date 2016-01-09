e85c7d6add998db75c4397c8d6545198cc0f52f1
erikvw <ew2789@gmail.com>
2016-01-09 19:51:32 +0200

    remove hardcoded db_table

--- edc/subject/code_lists/models/main.py
+++ edc/subject/code_lists/models/main.py
@@ -6,79 +6,77 @@ class WcsDxAdult(BaseCodeList):
 
     """WhoClinicalStagingDxAdult"""
 
-    list_ref = models.CharField("List Reference",
+    list_ref = models.CharField(
+        verbose_name="List Reference",
         max_length=100,
-        blank=True,
-        )
+        blank=True)
 
     class Meta:
         app_label = "code_lists"
-        db_table = "bhp_code_lists_wcsdxadult"  # TODO: remove when db schema is changed
 
 
 class WcsDxPed(BaseCodeList):
 
     """WhoClinicalStagingDxPediatric"""
 
-    list_ref = models.CharField("List Reference",
+    list_ref = models.CharField(
+        verbose_name="List Reference",
         max_length=100,
-        blank=True,
-        )
+        blank=True)
 
     class Meta:
         app_label = "code_lists"
-        db_table = "bhp_code_lists_wcsdxped"  # TODO: remove when db schema is changed
 
 
 class MedicationCode (BaseCodeList):
-    list_ref = models.CharField("List Reference",
+    list_ref = models.CharField(
+        verbose_name="List Reference",
         max_length=35)
 
     class Meta:
         app_label = "code_lists"
-        db_table = "bhp_code_lists_medicationcode"  # TODO: remove when db schema is changed
 
 
 class BodySiteCode (BaseCodeList):
-    list_ref = models.CharField("List Reference",
+    list_ref = models.CharField(
+        verbose_name="List Reference",
         max_length=35)
 
     class Meta:
         app_label = "code_lists"
-        db_table = "bhp_code_lists_bodysitecode"  # TODO: remove when db schema is changed
 
 
 class OrganismCode (BaseCodeList):
-    list_ref = models.CharField("List Reference",
+    list_ref = models.CharField(
+        verbose_name="List Reference",
         max_length=35)
 
     class Meta:
         app_label = "code_lists"
-        db_table = "bhp_code_lists_organismcode"  # TODO: remove when db schema is changed
 
 
 class ArvCode (BaseCodeList):
-    list_ref = models.CharField("List Reference",
+    list_ref = models.CharField(
+        verbose_name="List Reference",
         max_length=35)
 
     class Meta:
         app_label = "code_lists"
-        db_table = "bhp_code_lists_arvcode"  # TODO: remove when db schema is changed
 
 
 class ArvDoseStatus (BaseCodeList):
-    list_ref = models.CharField("List Reference",
+    list_ref = models.CharField(
+        verbose_name="List Reference",
         max_length=35)
 
     class Meta:
         app_label = "code_lists"
-        db_table = "bhp_code_lists_arvdosestatus"  # TODO: remove when db schema is changed
 
 
 class ArvModificationCode (BaseCodeList):
-    list_ref = models.CharField("List Reference",
+    list_ref = models.CharField(
+        verbose_name="List Reference",
         max_length=35)
 
     class Meta:
         app_label = "code_lists"
-        db_table = "bhp_code_lists_arvmodificationcode"  # TODO: remove when db schema is changed
--- edc/subject/code_lists/models/ssx_code.py
+++ edc/subject/code_lists/models/ssx_code.py
@@ -1,11 +1,12 @@
 from django.db import models
+
 from .base_code_list import BaseCodeList
 
 
 class SsxCode (BaseCodeList):
-    list_ref = models.CharField("List Reference",
+    list_ref = models.CharField(
+        verbose_name="List Reference",
         max_length=35)
 
     class Meta:
         app_label = "code_lists"
-        db_table = "bhp_code_lists_ssxcode"
