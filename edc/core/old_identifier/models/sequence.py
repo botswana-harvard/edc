2cc7ca8a5989c03fd3e21ec76dc9e8d77893072e
erikvw <ew2789@gmail.com>
2016-01-11 15:09:09 +0200

    remove old folder

--- edc/core/old_identifier/models/sequence.py
+++
@@ -1,9 +0,0 @@
-from .base_sequence import BaseSequence
-
-
-class Sequence(BaseSequence):
-
-    class Meta:
-        app_label = 'identifier'
-        db_table = 'bhp_identifier_sequence'
-        ordering = ['id', ]
