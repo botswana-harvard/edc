c1b08895a1a98fef52655b60db70fe45e1c52d8f
erikvw <ew2789@gmail.com>
2016-01-12 06:58:03 +0200

    remove unused bhp_userprofile

--- edc/core/bhp_userprofile/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'initials')
-    search_fields = ['user__username', 'user__first_name', 'user__last_name']
-admin.site.register(UserProfile, UserProfileAdmin)
